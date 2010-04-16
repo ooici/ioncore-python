437d4ee5d4ca957b0a52293971aae4fa21fc2f57
Paul Hubbard <pfhubbard@ucsd.edu>
2010-04-16 15:59:59 -0700

    merge time

--- ion/services/coi/datastore.py
+++ ion/services/coi/datastore.py
@@ -9,38 +9,61 @@
 from twisted.python import log
 from twisted.internet import defer
 
+import pycassa
+
 from magnet.spawnable import Receiver
 from magnet.spawnable import send
 from magnet.spawnable import spawn
-from magnet.store import Store
 
 import ion.util.procutils as pu
 
-store = Store()
-
-datastore = Store()
 
 receiver = Receiver(__name__)
 selfid = None
 
+class Store:
+    def start(self):
+        log.msg('Connecting to Cassandra...')
+        cass_list = ['localhost:9160']
+        self.client = pycassa.connect(cass_list)
+        self.kvs = pycassa.ColumnFamily(self.client, 'Datasets', 'Catalog')
+        log.msg('connected OK.')
+
+    def get(self, key):
+        if not self.kvs:
+            log.err('Not connected!')
+            return None
+        try:
+            val = self.kvs.get(key)
+            log.msg('Key "%s":"%s"' % (key, val))
+            return(val['value'])
+        except:
+            log.msg('Key "%s" not found' % key)
+            return(None)
+
+    def put(self, key, value):
+        if not self.kvs:
+            log.err('Not connected!')
+            return None
+        log.msg('writing key %s value %s' % (key, value))
+        self.kvs.insert(key, {'value' : value})
+
+    def query(self, regex):
+        log.err('Missing code')
+        return None
+
+    def delete(self, key):
+        log.err('Missing code')
+        return None
+
+datastore = Store()
+
 @defer.inlineCallbacks
 def start():
     id = yield spawn(receiver)
+    datastore.start()
     store.put('datastore', id)
     selfid = id
-
-def op_put(args):
-    print 'in "put" with ', args
-    datastore.put(args['key'],args['value'])
-
-@defer.inlineCallbacks
-def op_get(args, replyto):
-    print 'in "get" with ', args, replyto
-    value = yield datastore.get(args['key'])
-    print "GET '"+args['key']+"'='"+value+"'"
-
-    receiver.send(pu.get_process_id(replyto), {'from':'id', 'return-value':value})
-
 
 def receive(content, msg):
     """
@@ -52,28 +75,15 @@ def receive(content, msg):
             "args": {'key1':'arg1', 'key2':'arg2'}
         }
     """
-    pu.log_message(__name__, content, msg)
-    if "op" in content:
-		if content["op"] == "START":
-			print 'Start message received'
-			return
-		elif content["op"] == "GET":
-			return op_get(content['content'],msg.reply_to)
-		elif content["op"] == "PUT":
-			return op_put(content['content'])
-#		else:
-#		    log.error("Receive() failed. Op call does not match a service", content)    
-#	else:
-    log.error("Receive() failed. Bad message", content)    
+
 
 receiver.handle(receive)
 
 def test_put():
-	start()
-	receiver.send(selfid, {'op':'PUT','args':{'key':'key1','value':'val1'}})
-	# send(1,{'op':'PUT','args':{'key':'key1','value':'val1'}})
-	
+    start()
+    receiver.send(selfid, {'op':'PUT','args':{'key':'key1','value':'val1'}})
+    # send(1,{'op':'PUT','args':{'key':'key1','value':'val1'}})
+
 def test_get():
     receiver.send(selfid, {'op':'GET','args':{'key':'key1'}})
-	# send(1, {'op':'GET','args':{'key':'key1'}})
-	
+    # send(1, {'op':'GET','args':{'key':'key1'}})
