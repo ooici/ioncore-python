71c500c5a4186538083a0fc533aa6df0e28b3aee
Paul Hubbard <pfhubbard@ucsd.edu>
2010-04-22 16:28:33 -0700

    moving cassandra code and test to data tree

--- ion/services/coi/datastore.py
+++ ion/services/coi/datastore.py
@@ -14,56 +14,12 @@ from magnet.spawnable import send
 from magnet.spawnable import spawn
 from magnet.store import Store
 
-#import pycassa
 import ion.util.procutils as pu
 from ion.services.base_service import BaseService, BaseServiceClient
 
 logging.basicConfig(level=logging.DEBUG)
 logging.debug('Loaded: '+__name__)
 
-class CassandraStore():
-    """
-    Store interface for interacting with the Cassandra key/value store
-    """
-    def started(self):
-        return(hasattr(self, 'kvs'))
-
-    def start(self):
-        if self.started():
-            return
-
-        logging.info('Connecting to Cassandra...')
-        cass_list = ['localhost:9160']
-        self.client = pycassa.connect(cass_list)
-        self.kvs = pycassa.ColumnFamily(self.client, 'Datasets', 'Catalog')
-        logging.info('connected OK.')
-
-    def get(self, key):
-        if not self.started():
-            logging.error('Not connected!')
-            return None
-        try:
-            val = self.kvs.get(key)
-            logging.info('Key "%s":"%s"' % (key, val))
-            return(val['value'])
-        except:
-            logging.info('Key "%s" not found' % key)
-            return(None)
-
-    def put(self, key, value):
-        if not self.started():
-            logging.error('Not connected!')
-            return None
-        logging.info('writing key %s value %s' % (key, value))
-        self.kvs.insert(key, {'value' : str(value)})
-
-    def query(self, regex):
-        logging.error('Missing code')
-        return None
-
-    def delete(self, key):
-        logging.error('Missing code')
-        return None
 
 class DatastoreService(BaseService):
     datastore = Store()
@@ -89,7 +45,8 @@ class DatastoreService(BaseService):
         logging.info('Datastore.get('+key+') = '+str(value))
         replyto = msg.reply_to
         logging.info('Datastore.get() replyto='+replyto)
-        yield pu.send_message(receiver, '', pu.get_process_id(replyto), 'result', {'value':value}, {})
+        yield pu.send_message(receiver, '', pu.get_process_id(replyto),
+                              'result', {'value':value}, {})
 
 
 # Direct start of the service as a process with its default name
@@ -97,11 +54,6 @@ receiver = Receiver(__name__)
 instance = DatastoreService(receiver)
 
 
-
-def pfh_test():
-    start()
-    receive({'op':'put','content':{'key':'key1','value':'val1'}}, None)
-    receive({'op':'get','content':{'key':'key1'}}, None)
 
 """
 from ion.services.coi import datastore as d
