b8d61bea1060bb499176d26fa079db6c51e9264e
Paul Hubbard <pfhubbard@ucsd.edu>
2010-04-22 16:21:02 -0700

    Moving test to data/test

---
+++ ion/data/test/test_datastore.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+@file ion/services/coi/test/test_datastore.py
+@author Michael Meisinger
+@test Protocol+factory test of datastore
+"""
+
+
+from twisted.trial import unittest
+import logging
+from uuid import uuid4
+
+from ion.services.coi.datastore import DatastoreService, CassandraStore
+
+class DatastoreTest(unittest.TestCase):
+    def setUp(self):
+        logging.basicConfig(level=logging.DEBUG, \
+                format='%(asctime)s %(levelname)s [%(funcName)s] %(message)s')
+        self.ds = CassandraStore()
+        self.ds.start()
+
+    def tearDown(self):
+        del self.ds
+
+    def _mkey(self):
+        return str(uuid4())
+
+    def test_get_404(self):
+        rc = self.ds.get(self._mkey())
+        self.failUnlessEqual(rc, None)
+
+    def test_write_only(self):
+        self.ds.put(self._mkey(), self._mkey())
+
+    def test_putget(self):
+        raise unittest.SkipTest('Waiting for write to work')
+        key = self._mkey()
+        value = 'val1'
+        self.ds.put(key, value)
+        b = self.ds.get(key)
+        self.failUnlessEqual(value, b)
