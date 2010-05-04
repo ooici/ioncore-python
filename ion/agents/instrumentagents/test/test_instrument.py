b96d854d7adbc8e77ffba814dee933076c06ae8b
Steve Foley <sfoley@ucsd.edu>
2010-05-04 12:39:16 -0700

    Updated for latest magnet code?

--- ion/agents/instrumentagents/test/test_instrument.py
+++ ion/agents/instrumentagents/test/test_instrument.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 """
-@file ion/services/cei/provisioner.py
+@file ion/agents/instrumentagents/test/test_instrument.py
 @author Michael Meisinger
 @author Stephen Pasco
 @author Steve Foley
-@package ion.agents service for provisioning operational units (VM instances).
+@todo Simplify spawn call?
+@todo Clean out unnecessary imports
+@todo Test registry of resource into registry
 """
 
 import logging
@@ -23,21 +25,25 @@ import ion.util.procutils as pu
 
 logging.basicConfig(level=logging.DEBUG)
 
-class TestInstrumentAgent(IonTestCase, RpcClient):
-    
-    def __init__(self):
-        RpcClient.__init__(self)
-    
+class TestInstrumentAgent(IonTestCase):
+
     @defer.inlineCallbacks
     def setUp(self):
         IonTestCase.setUp(self)
         self.store = yield Store()
-        yield self.attach()
+        self._startContainer()
+        self.rpc = RpcClient()
+        yield self.rpc.attach()
         
-    @defer.inlineCallbacks
     def tearDown(self):
-        yield IonTestCase.tearDown(self)
+        self._stopContainer()
     
+    @defer.inlineCallbacks
+    def testGetSBE49Capabilities(self):
+        """
+        Test the ability to gather capabilities from the SBE49 instrument
+        capabilities
+        """
     @defer.inlineCallbacks
     def testGetSetSBE49Params(self):
         """
@@ -54,33 +60,34 @@ class TestInstrumentAgent(IonTestCase, RpcClient):
     
         yield self.store.put('SBE49InstrumentAgent', svc_id)
     
-        response = yield self.rpc_send(svc_id, 'get', \
+        response = yield self.rpc.rpc_send(svc_id, 'get', \
                                        ('baudrate', 'outputformat'), {})
         self.assertEqual(response['content'], {'baudrate' : 9600,
                                                'outputformat' : 0})
-        response = yield self.rpc_send(svc_id, 'set', {'baudrate': 19200,
+        response = yield self.rpc.rpc_send(svc_id, 'set', {'baudrate': 19200,
                                             'outputformat': 1}, {})
         self.assertEqual(response['content'], {'baudrate' : 19200,
                                                'outputformat' : 1})        
-        response = yield self.rpc_send(svc_id, 'get', ('baudrate',
+        response = yield self.rpc.rpc_send(svc_id, 'get', ('baudrate',
                                                        'outputformat'), {})
         self.assertEqual(response['content'], {'baudrate' : 19200,
                                                'outputformat' : 1})       
      
-        response = yield self.rpc_send(svc_id, 'setLifecycleState',
+        response = yield self.rpc.rpc_send(svc_id, 'setLifecycleState',
                                        'undeveloped', {})
         self.assertEqual(response['content'], 'undeveloped')       
-        response = yield self.rpc_send(svc_id, 'getLifecycleState', '', {})
+        response = yield self.rpc.rpc_send(svc_id, 'getLifecycleState', '', {})
         self.assertEqual(response['content'], 'undeveloped')       
-        response = yield self.rpc_send(svc_id, 'setLifecycleState',
+        response = yield self.rpc.rpc_send(svc_id, 'setLifecycleState',
                                        'developed', {})
         self.assertEqual(response['content'], 'developed')       
-        response = yield self.rpc_send(svc_id, 'getLifecycleState', '', {})
+        response = yield self.rpc.rpc_send(svc_id, 'getLifecycleState', '', {})
         self.assertEqual(response['content'], 'developed')       
 
-
+"""
 def start():
     testset = TestInstrumentAgent()
     testset.setUp()
     testset.testGetSetSBE49Params()
     testset.tearDown()
+"""
