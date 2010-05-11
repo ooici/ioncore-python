913886de9be978acd4362fc62540352d52f242f7
Paul Hubbard <pfhubbard@ucsd.edu>
2010-05-11 10:05:21 -0700

    merging with master

--- ion/data/test/test_fetcher.py
+++ ion/data/test/test_fetcher.py
@@ -18,10 +18,24 @@ class DatastoreTest(IonTestCase):
     @defer.inlineCallbacks
     def setUp(self):
         yield self._startContainer()
+        services = [{'name':'fetcher', 'module':'ion.data.fetcher',
+                    'class': 'FetcherService'},]
+        yield self._spawnProcesses(services)
+
+        self.dest = yield self.procRegistry.get('fetcher')
+        self.fc = FetcherClient()
+        self.fc.attach()
 
     @defer.inlineCallbacks
     def tearDown(self):
         yield self._stopContainer()
+
+    @defer.inlineCallbacks
+    def _get_page(self, src_url):
+        logging.debug('sending request for "%s"...' % src_url)
+        res = yield self.fc.get_url(self.dest, src_url)
+        msg = res['value']
+        defer.returnValue(msg)
 
     @defer.inlineCallbacks
     def test_single_get(self):
@@ -29,16 +43,10 @@ class DatastoreTest(IonTestCase):
         Simplest test, fetch a fixed local page.
         @note Contenst of same in /var/www/tmp on amoeba.ucsd.edu
         """
-        services = [{'name':'fetcher', 'module':'ion.data.fetcher',
-                    'class': 'FetcherService'},]
-        yield self._spawnProcesses(services)
-
-        dest = yield self.procRegistry.get('fetcher')
-        logging.debug('fetcher: '+repr(dest))
-
-        fc = FetcherClient()
-        fc.attach()
-        logging.debug('sending request...')
-        res = yield fc.get_url(dest, 'http://amoeba.ucsd.edu/tmp/test1.txt')
-        msg = res['value'].strip()
+        res = yield self._get_page('http://amoeba.ucsd.edu/tmp/test1.txt')
+        msg = res.strip()
         self.failUnlessEqual(msg, 'Now is the time for all good men to come to the aid of their country.')
+
+    @defer.inlineCallbacks
+    def test_404(self):
+        self.failUnlessFailure(self._get_page('http://ooici.net/404-fer-sure'))
