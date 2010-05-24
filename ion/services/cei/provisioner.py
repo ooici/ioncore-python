e3d20cf88cf1e78991a479df195921f7efde257e
David LaBissoniere <labisso@uchicago.edu>
2010-05-24 16:36:10 -0500

    provision request record and ack functionality

--- ion/services/cei/provisioner.py
+++ ion/services/cei/provisioner.py
@@ -8,12 +8,15 @@
 """
 
 import logging
-from twisted.internet import defer
-from magnet.spawnable import Receiver
+import uuid
+from twisted.internet import defer, reactor
 
 from ion.services.base_service import BaseService
 from ion.core import base_process
 from ion.core.base_process import ProtocolFactory
+from ion.services.cei.dtrs import DeployableTypeRegistryClient
+from ion.services.cei.provisioner_store import ProvisionerStore
+from ion.services.cei import states
 
 class ProvisionerService(BaseService):
     """Provisioner service interface
@@ -23,13 +26,67 @@ class ProvisionerService(BaseService):
     declare = BaseService.service_declare(name='provisioner', version='0.1.0', dependencies=[])
 
     def slc_init(self):
-        # trigger 'start' of LoopingCall
-        pass
-
+        self.store = ProvisionerStore()
+    
+    @defer.inlineCallbacks
     def op_provision(self, content, headers, msg):
         """Service operation: Provision a taskable resource
         """
         logging.info("op_provision content:"+str(content))
+        
+        document, launch_record, node_records = yield self._expand_request(content)
+        
+        #eventually should have some kind of bulk insert
+        self.store.put_state(launch_record['launch_id'], 
+                launch_record['state'], launch_record)
+        for record in node_records:
+            self.store.put_state(record['node_id'], record['state'], record)
+
+        # now we can ACK the request as it is safe in datastore
+
+        # set up a callLater to fulfill the request after the ack. Would be
+        # cleaner to have explicit ack control.
+        reactor.callLater(0, self._fulfill_launch, document, launch_record, node_records)
+
+    def _fulfill_launch(self, document, launch, nodes):
+        logging.info('Time to fulfill this request, kids.')
+        
+    @defer.inlineCallbacks
+    def _expand_request(self, request):
+        """Validates request and transforms it into a document and node records.
+
+        Returns a tuple (document, launch record, node records).
+        """
+        try:
+            deployable_type = request['deployable_type']
+            nodes = request['nodes']
+            launch_id = request['launch_id']
+            subscribers = request['subscribers'] #what will this look like?
+        except KeyError:
+            logging.error('Request was malformed')
+            #TODO error handling, what?
+            yield defer.fail()
+
+        #TODO how to do this lookup once for the service?
+        dtrs_id = yield base_process.procRegistry.get("dtrs")
+        dtrs = DeployableTypeRegistryClient(pid=dtrs_id)
+
+        dt = yield dtrs.lookup(deployable_type, nodes)
+
+        doc = dt['document']
+        node_groups = dt['nodes']
+
+        launch_record = _one_launch_record(launch_id, deployable_type,
+                subscribers)
+
+        node_records = []
+        for (group_name, group) in node_groups.iteritems():
+            node_ids = group['id']
+            for node_id in node_ids:
+                node_records.append(_one_node_record(node_id, group, 
+                    group_name, launch_record))
+        result = (doc, launch_record, node_records)
+        yield defer.returnValue(result)
 
     def op_terminate(self, content, headers, msg):
         """Service operation: Terminate a taskable resource
@@ -61,6 +118,22 @@ class ProvisionerService(BaseService):
         logging.info("Receipt has been taken.  content:"+str(content))
          
 
+def _one_launch_record(launch_id, dt, subscribers, state=states.Requested):
+    return {'launch_id' : launch_id,
+            'deployable_type' : dt,
+            'subscribers' : subscribers,
+            'state' : state,
+            }
+
+def _one_node_record(node_id, group, group_name, launch, state=states.Requested):
+    return {'launch_id' : launch['launch_id'],
+            'node_id' : node_id,
+            'state' : state,
+            'state_desc' : None,
+            'site' : group['site'],
+            'allocation' : group['allocation'],
+            'ctx-name' : group_name,
+            }
         
 # Spawn of the process using the module name
 factory = ProtocolFactory(ProvisionerService)
---
+++ ion/services/cei/test/test_provisioner.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+"""
+@file ion/services/cei/test/test_provisioner.py
+@author David LaBissoniere
+@brief Test provisioner behavior
+"""
+
+import logging
+import uuid
+from twisted.internet import defer
+from magnet.container import Container
+from magnet.spawnable import spawn
+
+from ion.test.iontest import IonTestCase
+import ion.util.procutils as pu
+
+from ion.services.cei.provisioner import ProvisionerService
+
+def _new_id():
+    return str(uuid.uuid4())
+
+class ProvisionerServiceTest(IonTestCase):
+
+    @defer.inlineCallbacks
+    def setUp(self):
+        yield self._start_container()
+
+    @defer.inlineCallbacks
+    def tearDown(self):
+        yield self._stop_container()
+
+    @defer.inlineCallbacks
+    def test_provisioner(self):
+        messaging = {'cei':{'name_type':'worker', 'args':{'scope':'local'}}}
+        procs = [
+            {'name':'provisioner','module':'ion.services.cei.provisioner','class':'ProvisionerService'},
+            {'name':'dtrs','module':'ion.services.cei.dtrs','class':'DeployableTypeRegistryService'}
+        ]
+        yield self._declare_messaging(messaging)
+        supervisor = yield self._spawn_processes(procs)
+
+        pId = yield self.procRegistry.get("provisioner")
+
+        request = {'deployable_type' : 'base-cluster',
+                'launch_id' : _new_id(),
+                'nodes' : { 
+                    'head-node' : {
+                        'id' : [_new_id()],
+                        'site' : 'ec2-west',
+                        'allocation' : 'small',
+                    },
+                    'worker-node' : {
+                        'id' : [_new_id(), _new_id()],
+                        'site' : 'ec2-west',
+                        'allocation' : 'small',
+                    },
+                },
+                'subscribers' : [],
+                }
+        
+        yield supervisor.send(pId, "provision", request)
+
+        yield pu.asleep(5) #async wait
+
