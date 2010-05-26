cdb0cfdbff59cd5c86dc0c13bc607a71ab4b3002
Brian Fox <brianfox@Brian-Foxs-MacBook-Pro.local>
2010-05-26 09:25:52 -0700

    Bumped up pylint score and pyflakes pass.

--- ion/services/coi/logger.py
+++ ion/services/coi/logger.py
@@ -8,9 +8,7 @@
 
 import logging.config
 from twisted.internet import defer
-from magnet.spawnable import Receiver
 
-import ion.util.procutils as pu
 from ion.core.base_process import ProtocolFactory
 from ion.services.base_service import BaseService, BaseServiceClient
 
@@ -21,7 +19,11 @@ class LoggerService(BaseService):
     """
 
     # Declaration of service
-    declare = BaseService.service_declare(name='logger', version='0.1.0', dependencies=[])
+    declare = BaseService.service_declare(
+        name='logger', 
+        version='0.1.0', 
+        dependencies=[]
+    )
 
     def slc_init(self):
         logging.info("LoggingService initialized")
@@ -33,18 +35,20 @@ class LoggerService(BaseService):
     def op_logmsg(self, content, headers, msg):
         level = content.get('level','info')
         logmsg = content.get('msg','#NO MESSAGE#')
-        lfrom = headers.get('sender','')
-        ltime = content.get('logtime')
+	
+	# need to do something reasonable with these soon
+        # lfrom = headers.get('sender','')
+        # ltime = content.get('logtime')
 
-        if level=='debug':
+        if level == 'debug':
             logserv.debug(logmsg)
-        elif level=='info':
+        elif level == 'info':
             logserv.info(logmsg)
-        elif level=='warn':
+        elif level == 'warn':
             logserv.warn(logmsg)
-        elif level=='error':
+        elif level == 'error':
             logserv.error(logmsg)
-        elif level=='critical':
+        elif level == 'critical':
             logserv.critical(logmsg)
         else:
             logging.error('Invalid log level: '+str(level))
@@ -64,15 +68,19 @@ class LoggerClient(BaseServiceClient):
     def logmsg(self, level, msg, sender, logtime):
         yield self._check_init()
         
-        # do we or don't we trust/care about client timestamps?  If not,
-        # the logtime isn't needed. 
+        # do we or don't we trust/care about client 
+	# timestamps?  If not, the logtime isn't needed. 
 
-        cont={'level':level,'msg':msg,'sender':sender,'logtime':logtime}            
+        cont = {
+            'level':level,
+            'msg':msg,
+            'sender':sender,
+            'logtime':logtime
+        }            
         (content, headers, msg) = yield self.rpc_send('logmsg', cont)
         logging.info('Service reply: '+str(content))
         
         defer.returnValue(str(content))
-
 
 # Spawn of the process using the module name
 factory = ProtocolFactory(LoggerService)
@@ -80,7 +88,4 @@ factory = ProtocolFactory(LoggerService)
 """
 from ion.services.coi import logger
 spawn(logger)
-send(1,{'op':'logmsg','sender':'snd.1','content':{'level':'info','logtime':'120202112','msg':'Test logging'}})
 """
-
-
