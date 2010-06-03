31622d156ee1a841b75dda49eb0d8b24fd906be2
Paul Hubbard <pfhubbard@ucsd.edu>
2010-06-03 14:50:26 -0700

    merge

--- ion/play/rdf_store/test/test_rdf_service.py
+++ ion/play/rdf_store/test/test_rdf_service.py
@@ -26,27 +26,27 @@ class ReferenceTest(IonTestCase):
     @defer.inlineCallbacks
     def setUp(self):
         yield self._start_container()
-        
+
         print 'Started Container'
         self.rdfs=RdfStore()
         yield self.rdfs.init()
-        
+
         self.key='key1'
         self.ref1='ref1'
         self.ref2='ref2'
         self.ref3='ref3'
         self.ref4='ref4'
-        
+
     @defer.inlineCallbacks
     def tearDown(self):
         yield self._stop_container()
 
     @defer.inlineCallbacks
     def test_Use_Multiple_Clients(self):
-        
+
         res = yield self.rdfs.ssc_ref.add_reference(self.key,self.ref1)
         self.assertEqual(res,self.key)
-        
+
 
         res = yield self.rdfs.ssc_ref.add_reference(self.key,self.ref2)
         res = yield self.rdfs.ssc_ref.add_reference(self.key,self.ref4)
@@ -55,12 +55,12 @@ class ReferenceTest(IonTestCase):
 
         res = yield self.rdfs.ssc_ref.get_references(self.key)
         self.assertEqual(set(res),set([self.ref1,self.ref2,self.ref3,self.ref4]))
-        
+
         res = yield self.rdfs.ssc_ref.del_reference(self.key,self.ref1)
         self.assertEqual(res,'success')
-        
-        
+
+
         res = yield self.rdfs.ssc_ref.get_references(self.key)
         self.assertEqual(set(res),set([self.ref2,self.ref3,self.ref4]))
-        
+
         
