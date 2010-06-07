bbeba57f620abc8ff14be33069266368f93bb169
Tim Freeman <tim@timfreeman.org>
2010-06-06 19:41:38 -0500

    controller and N-preserving decision engine, needs messaging and testing, all I could do in time allotted

---
+++ ion/services/cei/decisionengine/default/__init__.py
@@ -0,0 +1,2 @@
+__all__ = ["DefaultEngine"]
+from default_engine import DefaultEngine
--- ion/services/cei/epu_controller.py
+++ ion/services/cei/epu_controller.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 
-"""
-@file ion/services/cei/epu_controller.py
-@author Alex Clemesha
-@brief Evaluate data from Sensor Aggregator against policies and take compensation actions.
-"""
-
 import logging
 from twisted.internet import defer
 from magnet.spawnable import Receiver
 from ion.services.base_service import BaseService
 from ion.core.base_process import ProtocolFactory
+from ion.services.cei.epucontroller import ControllerCore
 
 logging.basicConfig(level=logging.DEBUG)
 logging.debug('Loaded: '+__name__)
@@ -18,16 +13,37 @@ logging.debug('Loaded: '+__name__)
 class EPUControllerService(BaseService):
     """EPU Controller service interface
     """
+    
     declare = BaseService.service_declare(name='epu_controller', version='0.1.0', dependencies=[])
+    
+    def __init__(self):
+        self.provisioner_client = ProvisionerClient(self)
+        
+    def slc_init(self):
+        # todo: make this class configurable
+        engineclass = "ion.services.cei.decisionengine.default.DefaultEngine"
+        self.core = ControllerCore(self.provisioner_client, engineclass)
+        
+        # TODO: Twistify this
+        self.core.begin_controlling()
 
     @defer.inlineCallbacks
     def op_sensor_aggregator_info(self, content, headers, msg):
-        """
-        Take in SensorAggregator info, and make Policy decisions (via the "Policy Engine").
+        self.core.new_sensor_info(content)
 
-        """
-        logging.info("content:"+str(content))
-        yield self.reply(msg, 'result', {'result':'policy-executed-success'}, {})        
+class ProvisionerClient(object):
+    """Abstraction for sending messages to the provisioner.
+    """
+
+    def __init__(self, process):
+        self.process = process
+
+    @defer.inlineCallbacks
+    def provision(self, launch_id, launch_description):
+        logging.debug("Sending provision request, id '%s', content '%s'" % (launch_id, launch_description))
+        
+        # TODO
+        #yield self.process.send(sub, operation, record)
 
 # Direct start of the service as a process with its default name
 factory = ProtocolFactory(EPUControllerService)
