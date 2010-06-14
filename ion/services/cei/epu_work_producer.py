0ade4125841c5a9561a561efddcbff87323dedf0
Alex Clemesha <clemesha@ucsd.edu>
2010-06-14 15:15:16 -0700

    EPUWorkProducerClient: Client for sending the "start_producing" message to the EPUWorkProducer

--- ion/services/cei/epu_work_producer.py
+++ ion/services/cei/epu_work_producer.py
@@ -4,7 +4,7 @@ import logging
 import random
 from twisted.internet import defer
 from magnet.spawnable import Receiver
-from ion.services.base_service import BaseService
+from ion.services.base_service import BaseService, BaseServiceClient
 from ion.core.base_process import ProtocolFactory
 
 logging.basicConfig(level=logging.DEBUG)
@@ -14,7 +14,6 @@ class EPUWorkProducer(BaseService):
     """EPU Work Producer.
     """
     declare = BaseService.service_declare(name='epu_work_producer', version='0.1.0', dependencies=[])
-    work_produce_interval = 1 #in seconds
 
     def slc_init(self):
         self.queue_name_work = self.spawnArgs["queue_name_work"]
@@ -22,12 +21,33 @@ class EPUWorkProducer(BaseService):
 
     def op_start_producing(self, content, headers, msg):
         logging.info("op_start_producing")
-        self.work_produce_loop.start(self.work_produce_interval)
+        work_produce_interval = content["work_produce_interval"]
+        self.work_produce_loop.start(work_produce_interval)
 
     @defer.inlineCallbacks
     def work_produce(self): 
         work_amount = random.randint(1, 10) # "work", aka sleep for given seconds
         yield self.send(self.queue_name_work, 'work', {"work_amount":work_amount})
+
+
+class EPUWorkProducerClient(BaseServiceClient):
+    """Client for sending the "start_producing" message to
+    the EPUWorkProducer to start sending work messages to the EPU Workers.
+    """
+
+    def __init__(self, work_produce_interval=1, proc=None, **kwargs):
+        if not 'targetname' in kwargs:
+            kwargs['targetname'] = "epu_work_producer"
+        BaseServiceClient.__init__(self, proc, **kwargs)
+        self.work_produce_interval = work_produce_interval
+
+    @defer.inlineCallbacks
+    def start_work_producer(self):
+        """Send message to epu_work_producer to make it start producing work"""
+        yield self._check_init()
+        logging.debug('Commanding epu_work_producer to start producing')
+        request = {"work_produce_interval":self.work_produce_interval}
+        yield self.send('start_producing', request)
  
 
 # Direct start of the service as a process with its default name
