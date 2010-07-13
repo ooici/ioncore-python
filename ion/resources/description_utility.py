89190f5bda96748516bd090f489552b16489ad97
David Stuebe <dstuebe@asascience.com>
2010-07-12 22:15:13 -0400

    Aadded resource decscription loader.

---
+++ ion/resources/description_utility.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import logging
+logging = logging.getLogger(__name__)
+from ion.core import ioninit
+CONF = ioninit.config(__name__)
+import inspect
+from ion.data import dataobject
+
+
+def load_descriptions():
+
+    modules=CONF.getObject()
+    if not modules:
+        logging.warning('Found no Resource Description modules to load!')
+
+    for module in modules:
+        print module
+        mod = __import__(module)
+
+        for name in dir(mod):
+            obj = getattr(mod, name)
+            if inspect.isclass(obj):
+                if issubclass(obj, dataobject.DataObject):
+                    dataobject.DataObject._types[name]=obj
+
+
