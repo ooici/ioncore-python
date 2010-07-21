21d4430924a2426a95e36de2041692e83b50721d
Brian Fox <bfox@ucsd.edu>
2010-07-21 11:36:49 -0700

    Setting up services.

--- ion/services/coi/host_status/host_status_client.py
+++
@@ -1,7 +0,0 @@
-import xmlrpclib, json
-
-s = xmlrpclib.ServerProxy('http://localhost:9010')
-print json.dumps(s.getStatus(), indent=4)
-
-# Print list of available methods
-print s.system.listMethods()
--- ion/services/coi/test/test_host_status.py
+++ ion/services/coi/test/test_host_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 """
-@file ion/services/coi/test/test_host_status_rpc.py
+@file ion/services/coi/test/test_host_status.py
 @author Brian Fox
-@brief test rpc portion of the host status classes
+@brief test service for sending host_ tatus messages
 """
 
 import logging
@@ -11,30 +11,47 @@ logging = logging.getLogger(__name__)
 from twisted.internet import defer
 from twisted.trial import unittest
 
-import json
+from ion.services.coi.service_registry import ServiceDesc, ServiceRegistryClient,\
+ ServiceInstanceDesc
 from ion.test.iontest import IonTestCase
-from ion.services.coi.host_status import SnmpReader,HostStatus
 
 class HostStatusTest(IonTestCase):
     """
+    Testing client classes of host_status
     """
+    @defer.inlineCallbacks
     def setUp(self):
-        pass
+        yield self._start_container()
+        #self.sup = yield self._start_core_services()
+        services = [
+            {'name':'hoststatus1','module':'ion.services.coi.host_status','class':'HostStatusService'}]
+        sup = yield self._spawn_processes(services)
+        self.src = ServiceRegistryClient(proc=sup)
 
+
+    @defer.inlineCallbacks
     def tearDown(self):
-        pass
-    
-    def test_DeliberatelyBadSnmpReader(self):
-        hs = HostStatus('localhost', 180, 'ccagent', 'ooicinet',timeout=0.5, retries=0)
-        all = json.dumps(hs.getAll(), indent=4)
-        print all
-        
-    def test_SnmpReader(self):
-        hs = HostStatus('localhost', 161, 'ccagent', 'ooicinet')
-        all = json.dumps(hs.getAll(), indent=4)
-        print all
-                
-        
+        yield self._stop_container()
 
+
+    @defer.inlineCallbacks
+    def test_service_reg(self):
+        pass
+        sd1 = ServiceDesc()
+        sd1.svc_name='svc1'
+        sd1.name = 'service'
+        
+        res1 = yield self.src.register_service(sd1)
+
+        si1 = ServiceInstanceDesc()
+        si1.xname='self.sup.id.full'
+        si1.svc_name='svcinst1'
+        si1.name = 'service instance'
+        
+        ri1 = yield self.src.register_service_instance(si1)
+
+        # Return string with xname
+        ri2 = yield self.src.get_service_instance_name('svcinst1')
+        self.assertEqual(ri2, 'self.sup.id.full')
 
 
