4420f0cfc8c0c3ab41647672b67983550881426a
Steve Foley <sfoley@ucsd.edu>
2010-07-21 14:18:41 -0700

    Work in progress with Stuebe

--- ion/agents/test/test_resource_agent.py
+++ ion/agents/test/test_resource_agent.py
@@ -15,14 +15,16 @@ from ion.test.iontest import IonTestCase
 from ion.agents.resource_agent import ResourceAgentClient
 from ion.services.coi.resource_registry import ResourceRegistryClient
 from ion.resources.ipaa_resource_descriptions \
-    import InstrumentAgentResourceDescription
+    import InstrumentAgentResourceDescription, InstrumentAgentResourceInstance
+from ion.data.dataobject import InformationResource, TypedAttribute
+
 class TestResourceAgent(IonTestCase):
     
     @defer.inlineCallbacks
     def setUp(self):
         yield self._start_container()
         
-        # us some sort of agent...doesnt really matter what kind, does it
+        # use some sort of agent...doesnt really matter what kind, does it
         processes = [
             {'name':'testAgent',
              'module':'ion.agents.instrumentagents.SBE49_IA',
@@ -39,22 +41,60 @@ class TestResourceAgent(IonTestCase):
         self.RAClient = ResourceAgentClient(proc=self.sup, target=self.svc_id)
         # RR Client is ,,,,,desired only for testing
         self.RRClient = ResourceRegistryClient(proc=self.sup, target=self.rr_id)
-
+        yield self.RRClient.clear_registry()
+        
         # setup the resource registry client
         yield self.RAClient.set_resource_registry_client(str(self.rr_id))
 
-        self.res_desc = InstrumentAgentResourceDescription()
-        self.res_desc.name = "testAgent"
+        """
+        res_to_describe = dataobject.InformationResource.create_new_resource()
+        res_description = yield self.rrc.register_resource_definition(res_to_describe)
+        ref = yield self.rrc.set_resource_lcstate_commissioned(res_description)
+        commissioned_description = yield self.rrc.get_resource_definition(ref)
+        """
+        
+        """
+        I want to:
+        - create a resource description with specific fields (where do I inherit from?)
+        - create an instance of that description with some data specific to my instance
+        - Link the instance to the description
+        - register the resource description and instance
+        - Change the lifecycle state of the resource, get it to confirm
+        
+        """
+        # need any non-generic resource...use an instrument agent one for now
+        self.res_desc = InstrumentAgentResourceDescription.create_new_resource()
+        self.resource_desc.driver_class = TypedAttribute( \
+            "ion.agent.instrumentagents.SBE49_instrument_driver.SBE49InstrumentDriver")
+        
+        self.res_inst = InstrumentAgentResourceInstance.create_new_resource()
+        self.res_inst.driver_process_id = "something_for_now.1"
+        
+        logging.debug("*** resource: %s", self.resource)
         
     @defer.inlineCallbacks
     def tearDown(self):
         yield self._stop_container()
         
     @defer.inlineCallbacks
-    def test_reg_direct(self):
-        id = str(uuid.uuid4())
-        result = yield self.RRClient.register_resource(id,self.res_desc)
-        self.assertEqual(result, id)
+    def test_reg_setstate_direct(self):
+        result = yield self.RRClient.register_resource_definition(self.res_desc)
+        logging.debug("*** register_def result: %s", result)
+        result = yield self.RRClient.register_resource_instance(self.res_inst)
+        logging.debug("*** register_inst result: %s", result)        
+        result = yield self.RRClient.get_resource_definition()
+        logging.debug("*** get_def result: %s", result)        
+        result = yield self.RRClient.get_resource_instance()
+        logging.debug("*** get_inst result: %s", result)        
+
+        # assert some stuff
+        result = yield self.RRClient.set_resource_lcstate(self.res_inst, LCState.active)
+        result = yield self.RRClient.get_resource_instance()
+        # get the state out of this somehow and assert stuff
+        result = yield self.RRClient.set_resource_lcstate(self.res_inst, LCState.active)
+        result = yield self.RRClient.set_resource_lcstate(self.res_inst, LCState.developed)
+        result = yield self.RRClient.set_resource_lcstate(self.res_inst, LCState.active)
+
         
     @defer.inlineCallbacks
     def test_registration(self):
--- ion/resources/ipaa_resource_descriptions.py
+++ ion/resources/ipaa_resource_descriptions.py
@@ -8,12 +8,28 @@ Definitions for resource descriptions used within the IPAA subsystem
 from ion.data.dataobject import DataObject, Resource, TypedAttribute, LCState, LCStates, ResourceReference, InformationResource, StatefulResource
 from ion.resources.coi_resource_descriptions import ResourceDescription 
 
-class InstrumentAgentResourceDescription(ResourceDescription):
-    """
-    The derived class that specifies what information the instrument agents
-    are going to keep in the resource registry
-    @note <class> must be a type which Python can instantiate with eval!
+class AgentMethodInterface(StatefulResource):
+    description = TypedAttribute(str)
+    arguments = TypedAttribute(str)
 
-    att1 = TypedAttribute(<class>, default=None)
-    att2 = TypedAttribute(<class>)
+class AgentDescription(StatefulResource):
     """
+    Resource Descriptions are stored in the resource registry.
+    They describe resources, resource types and resource attributes
+    """
+    interface = TypedAttribute(list)
+    module = TypedAttribute(str)
+    version = TypedAttribute(str)
+    #spawnargs = TypedAttribute(dict,{})
+    description = TypedAttribute(str)
+    
+class AgentInstance(StatefulResource):
+    """
+    Resource Instances are stored in the resource registry.
+    They describe instances of a resource type
+    """
+    description = TypedAttribute(ResourceReference)
+    #owner = TypedAttribute(ResourceReference)
+    spawnargs = TypedAttribute(str)
+    type = TypedAttribute(str)
+    process_id = TypedAttribute(str)
