7ab3d6b573f2e0ea791786289f83ef205acafc45
Brian Fox <bfox@ucsd.edu>
2010-07-23 11:34:00 -0700

    Fleshing out service.

--- ion/services/coi/test/test_host_status.py
+++ ion/services/coi/test/test_host_status.py
@@ -6,52 +6,64 @@
 @brief test service for sending host_ tatus messages
 """
 
-import logging
+import logging, subprocess, sys
 logging = logging.getLogger(__name__)
+
 from twisted.internet import defer
 from twisted.trial import unittest
+from twisted.internet.task import LoopingCall
 
-from ion.services.coi.service_registry import ServiceDesc, ServiceRegistryClient,\
- ServiceInstanceDesc
+from ion.services.coi.hostsensor.readers import HostReader
 from ion.test.iontest import IonTestCase
+import ion.util.procutils as pu
+
 
 class HostStatusTest(IonTestCase):
     """
     Testing client classes of host_status
     """
+    HOST_STATUS_DAEMON = 'ion/services/coi/hostsensor/host_status_daemon.py'
+    
     @defer.inlineCallbacks
     def setUp(self):
+        logging.debug('Starting Host Status XMLRPC Server')
+        p = subprocess.Popen(
+                     [sys.executable, self.HOST_STATUS_DAEMON,'start'], 
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.PIPE
+                     )
+        # wait 5 seconds for the daemon to shutdown
+        retries = 5
+        while not p.returncode == None and retries > 0:
+            logging.debug('Waiting for server to start')
+            yield pu.asleep(1)
+            retries -= 1
+        logging.debug('Server started with returncode %s'%str(p.returncode))
         yield self._start_container()
-        #self.sup = yield self._start_core_services()
-        services = [
-            {'name':'hoststatus1','module':'ion.services.coi.host_status','class':'HostStatusService'}]
-        sup = yield self._spawn_processes(services)
-        self.src = ServiceRegistryClient(proc=sup)
 
 
     @defer.inlineCallbacks
     def tearDown(self):
+        p = subprocess.Popen(
+                     [sys.executable, self.HOST_STATUS_DAEMON,'stop'], 
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.PIPE
+                     )
+        # wait 5 seconds for the daemon to shutdown
+        retries = 5
+        while not p.returncode == None and retries > 0:
+            yield pu.asleep(1)
+            retries -= 1
         yield self._stop_container()
 
 
+
     @defer.inlineCallbacks
-    def test_service_reg(self):
-        pass
-        sd1 = ServiceDesc()
-        sd1.svc_name='svc1'
-        sd1.name = 'service'
-        
-        res1 = yield self.src.register_service(sd1)
-
-        si1 = ServiceInstanceDesc()
-        si1.xname='self.sup.id.full'
-        si1.svc_name='svcinst1'
-        si1.name = 'service instance'
-        
-        ri1 = yield self.src.register_service_instance(si1)
-
-        # Return string with xname
-        ri2 = yield self.src.get_service_instance_name('svcinst1')
-        self.assertEqual(ri2, 'self.sup.id.full')
-
+    def test_BasicService(self):
+        # Test with seperate store backends
+        services = [
+            {'name':'hoststatus1','module':'ion.services.coi.host_status','class':'HostStatusService','spawnargs':{}}
+        ]
+        sup = yield self._spawn_processes(services)
+        yield pu.asleep(10)
 
