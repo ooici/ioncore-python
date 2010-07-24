8b4040a2c71b280c1026757a03551ff0e1ad7ee4
David Stuebe <dstuebe@asascience.com>
2010-07-23 20:54:34 -0400

    Started on Dataset Resource

--- ion/resources/dm_resource_descriptions.py
+++ ion/resources/dm_resource_descriptions.py
@@ -11,3 +11,61 @@ class EXAMPLE_RESOURCE(ResourceDescription):
     att2 = TypedAttribute(<class>)
 """
 
+
+
+
+class PerservationServiceResource(StatefulResource): # Is it stateful or information?
+    logical_name = TypedAttribute(str)
+    datatype = TypedAttribute(str)
+    archived_locations = TypedAttribute(list) # List of PreservationLocation objects
+
+class PreservationLocation(DataObject):
+    location = TypedAttribute(str)
+
+
+class CDMDatasetResource(InformationResource):
+    '''
+    @Note <class> must be a type which python can instantiate with eval!
+    '''
+    groups = TypedAttribute(list)
+    preservation_archive = TypedAttribute(PerservationServiceResource)
+    
+class CDMGroupResource(InformationResource):
+    attributes = TypedAttribute(list)
+    dimensions = TypedAttribute(list)
+    variables = TypedAttribute(list)
+    
+class TypedObject(DataObject):
+    """
+    """
+    
+class CDMAttributeResource(InformationResource):
+    attname = TypedAttribute(str)
+    value = TypedAttribute(TypedObject)
+    
+
+    
+class FloatObject(TypedObject):
+    f = TypedAttribute(float)
+    
+class IntegerObject(TypedObject):
+    i = TypedAttribute(int)
+    
+class StringObject(TypedObject):
+    s = TypedAttribute(str)
+    
+    
+    
+class CDMDimensionResource(InformationResource):
+    dim = TypedAttribute(int,0)
+    dimname = TypedAttribute(str)
+    unlimited = TypedAttribute(bool,False)
+    
+class CDMVariableResource(InformationResource):
+    """
+    """
+class CDMStructureResource(InformationResource):
+    """
+    """
+    
+    
