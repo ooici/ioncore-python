a6ded331e1d6ffe7dc8496b066259fdc59967849
David Stuebe <dstuebe@asascience.com>
2010-07-26 16:18:34 -0400

    Working on dataset/pubsub resources and registry

--- ion/resources/dm_resource_descriptions.py
+++ ion/resources/dm_resource_descriptions.py
@@ -11,13 +11,49 @@ class EXAMPLE_RESOURCE(ResourceDescription):
     att2 = TypedAttribute(<class>)
 """
 
+"""
+DM Pub Sub Registry Resource Descriptions
+"""
+
+class PublicationResource(StatefulResource):
+    """
+    A registry object which contai
+    """
+    #Name - inherited!
+    queue = TypedAttribute(str)
+    topics = TypedAttribute(list) # List of refs for registered topics
+    content_type = TypedAttribute()
+    
+class PubSubTopic(InformationResource):
+    """
+    """
+    #Name - inherited
+    keywords = TypedAttribute(str)
+    aoi = TypedAttribute(AOI)
+
+class SubscriptionResource(StatefulResource):
+    """
+    """
+    #Name - inherited
+    queue = TypedAttribute(str)
+    topics = TypedAttribute(list)
+    period = TypedAttribute(list)
+    interval = TypedAttribute(int,0)
 
 
+class AOI(DataObject):
+    """
+    Implement class and comparison methods for AOI!
+    """
 
+
+"""
+DM DataSet Resource Descriptions 
+"""
 class PerservationServiceResource(StatefulResource): # Is it stateful or information?
-    logical_name = TypedAttribute(str)
+    #Name (Logical IRODS Name) - inherited
     datatype = TypedAttribute(str)
-    archived_locations = TypedAttribute(list) # List of PreservationLocation objects
+    archived_locations = TypedAttribute(list) # List of Preservation Location objects
 
 class PreservationLocation(DataObject):
     location = TypedAttribute(str)
@@ -27,45 +63,72 @@ class CDMDatasetResource(InformationResource):
     '''
     @Note <class> must be a type which python can instantiate with eval!
     '''
+    #Name - inherited
     groups = TypedAttribute(list)
     preservation_archive = TypedAttribute(PerservationServiceResource)
     
 class CDMGroupResource(InformationResource):
+    #Name - inherited
     attributes = TypedAttribute(list)
     dimensions = TypedAttribute(list)
     variables = TypedAttribute(list)
+    preservation_archive = TypedAttribute(PerservationServiceResource)
+    
+class CDMAttributeResource(InformationResource):
+    #Name - inherited
+    value = TypedAttribute(TypedObject)
+    preservation_archive = TypedAttribute(PerservationServiceResource)
+    archive_attid = TypedAttribute(int,0) # Varid or name?
+    
+class CDMDimensionResource(InformationResource):
+    #Name - inherited
+    dim = TypedAttribute(int,0)
+    unlimited = TypedAttribute(bool,False)
+    shared = TypedAttribute(bool,False)
+    is_variable_length = TypedAttribute(bool,False)
+    preservation_archive = TypedAttribute(PerservationServiceResource)
+    archive_dimid = TypedAttribute(int,0) # Varid or name?
+    
+class CDMVariableResource(InformationResource):
+    """
+    """
+    #Name - inherited
+    attributes = TypedAttribute(list)
+    dimensions = TypedAttribute(list)
+    type = TypedAttribute()
+    preservation_archive = TypedAttribute(PerservationServiceResource)
+    archive_varid = TypedAttribute(str) # Varid or name?
+    
+class CDMStructureResource(InformationResource):
+    """
+    """
+    #Name - inherited
+    members = TypedAttribute(list)
     
 class TypedObject(DataObject):
     """
     """
     
-class CDMAttributeResource(InformationResource):
-    attname = TypedAttribute(str)
-    value = TypedAttribute(TypedObject)
-    
-
-    
 class FloatObject(TypedObject):
+    """
+    #@Todo convert to use numpy types
+    """
     f = TypedAttribute(float)
     
 class IntegerObject(TypedObject):
+    """
+    #@Todo convert to use numpy types
+    """
     i = TypedAttribute(int)
     
 class StringObject(TypedObject):
+    """
+    #@Todo convert to use numpy types
+    """
     s = TypedAttribute(str)
     
     
     
-class CDMDimensionResource(InformationResource):
-    dim = TypedAttribute(int,0)
-    dimname = TypedAttribute(str)
-    unlimited = TypedAttribute(bool,False)
-    
-class CDMVariableResource(InformationResource):
-    """
-    """
-class CDMStructureResource(InformationResource):
-    """
-    """
+
     
     
--- ion/services/dm/datapubsub.py
+++ ion/services/dm/datapubsub.py
@@ -14,6 +14,74 @@ from ion.core import bootstrap
 from ion.core.base_process import ProtocolFactory
 from ion.services.base_service import BaseService, BaseServiceClient
 
+
+class DataPubSubRegistryService(registry.BaseRegistryService):
+    """Dataset registry service interface
+    """
+ 
+     # Declaration of service
+    declare = BaseService.service_declare(name='datapubsub_registry', version='0.1.0', dependencies=[])
+
+    op_  = registry.BaseRegistryService.base_register_resource
+    """
+    Service operation: Create or update a dataset resource.
+    """
+    op_get_dataset = registry.BaseRegistryService.base_get_resource
+    """
+    Service operation: Get dataset description
+    """
+    op_find_dataset = registry.BaseRegistryService.base_find_resource
+    """
+    Service operation: Find dataset resource by characteristics
+    """
+
+
+        
+# Spawn of the process using the module name
+factory = ProtocolFactory(DatasetRegistryService)
+
+
+class DatasetRegistryClient(registry.BaseRegistryClient):
+    """
+    Class for the client accessing the resource registry.
+    """
+    def __init__(self, proc=None, **kwargs):
+        if not 'targetname' in kwargs:
+            kwargs['targetname'] = "dataset_registry"
+        BaseServiceClient.__init__(self, proc, **kwargs)
+
+    
+    def clear_registry(self):
+        return self.base_clear_registry('clear_registry')
+
+
+    def define_dataset(self,dataset):
+        """
+        @Brief Client method to Register a Dataset
+        
+        @param dataset is an instance of a dataset resource
+        """
+        return  self.base_register_resource('define_dataset', dataset)    
+
+    
+    def get_dataset(self,dataset_reference):
+        """
+        @Brief Get a dataset by reference
+        @param dataset_reference is the unique reference object for a registered
+        dataset
+        """
+        return self.base_get_resource(dataset_reference,'get_dataset')
+        
+    def find_dataset(self, description,regex=True,ignore_defaults=True,attnames=[]):
+        """
+        @Brief find all registered datasets which match the attributes of description
+        @param see the registry docs for params
+        """
+        return self.base_find_resource('find_dataset',description,regex,ignore_defaults,attnames)
+
+
+
+
 class DataPubsubService(BaseService):
     """Data publish/subscribe service interface
     """
