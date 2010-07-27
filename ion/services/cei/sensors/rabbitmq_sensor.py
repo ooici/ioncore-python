43eec11b40647d609319bd231875ba7f3746f790
Alex Clemesha <clemesha@ucsd.edu>
2010-07-27 14:56:03 -0700

    Improved 'rabbitmq_sensor.py' to properly use a remote RabbitMQ broker. Also updated 'chefroles' rabbitmq_sensor service entry to show new config params.

--- ion/services/cei/sensors/rabbitmq_sensor.py
+++ ion/services/cei/sensors/rabbitmq_sensor.py
@@ -17,24 +17,25 @@ class RabbitMQSensor(SensorProcess):
         SensorProcess.plc_init(self)
         self.queue_name_work = self.get_scoped_name("system", self.spawn_args["queue_name_work"])
         self.queue_name_events = self.get_scoped_name("system", self.spawn_args["queue_name_events"])
-        self.sensor_client = self._create_sensor_client()
+
+        erlang_cookie = self.spawn_args.get("erlang_cookie", None)
+        if erlang_cookie is None:
+            cookiefile = open(os.path.expanduser("~/.erlang.cookie"))
+            erlang_cookie = cookiefile.read().strip()
+            cookiefile.close()
+        rabbitmq_node = self.spawn_args.get("rabbitmq_node", "rabbit@localhost") 
+
+        self.sensor_client = self._create_sensor_client(erlang_cookie, rabbitmq_node)
         self.sensor_loop = LoopingCall(self.messages_in_queue)
         if self.start_immediately:
             self.sensor_loop.start(self.sensor_interval)
 
-    def _create_sensor_client(self, erlang_cookie="/home/cc/.erlang.cookie", nodename="txrabbitmq"):
-        #XXX Move the below into the 'txrabbitmq' library
+    def _create_sensor_client(self, erlang_cookie, rabbitmq_node):
         from txrabbitmq.service import RabbitMQControlService
-        from twotp.node import Process, readCookie, buildNodeName
-        if erlang_cookie is None:
-            cookie = readCookie()
-        else:
-            fh = open(erlang_cookie)
-            cookie = fh.read().strip()
-            fh.close()
-        nodeName = buildNodeName(nodename)
-        process = Process(nodeName, cookie)
-        return RabbitMQControlService(process)
+        from twotp.node import Process, buildNodeName
+        nodeName = buildNodeName(rabbitmq_node)
+        process = Process(nodeName, erlang_cookie)
+        return RabbitMQControlService(process, nodeName)
 
     @inlineCallbacks
     def op_stop(self, content, headers, msg):
