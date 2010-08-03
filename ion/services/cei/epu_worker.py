6d9a2d764f05e5eebbaf6ab40ebd4570b8d0fb32
Alex Clemesha <clemesha@ucsd.edu>
2010-08-02 17:15:58 -0700

    EPUWorkerService now does correct 'bootstrap.declare_messaging' and has 'slc_init' not 'plc_init', and thus is full of WIN..

--- ion/services/cei/epu_worker.py
+++ ion/services/cei/epu_worker.py
@@ -4,6 +4,7 @@ import time
 import logging
 from twisted.internet import defer
 from magnet.spawnable import spawn, Receiver
+from ion.core import bootstrap
 from ion.services.base_service import BaseService
 from ion.core.base_process import ProtocolFactory
 
@@ -16,19 +17,25 @@ class EPUWorkerService(BaseService):
     declare = BaseService.service_declare(name='epu_worker', version='0.1.0', dependencies=[])
 
     @defer.inlineCallbacks
-    def plc_init(self):
+    def slc_init(self):
         self.queue_name_work = self.get_scoped_name("system", self.spawn_args["queue_name_work"])
+        worker_queue = {self.queue_name_work:{'name_type':'worker'}}
+        yield bootstrap.declare_messaging(worker_queue)
+
         workReceiver = Receiver(__name__, self.queue_name_work)
         self.workReceiver = workReceiver
         self.workReceiver.handle(self.receive)
         spawnId = yield spawn(workReceiver)
 
+
     @defer.inlineCallbacks
     def op_work(self, content, headers, msg):
         logging.info("EPUWorkerService ---doing work--- content:"+str(content))
         time.sleep(content['work_amount'])
-        yield self.reply(msg, 'result', {'result':'work_complete'}, {})        
- 
+        yield self.reply(msg, 'result', {'result':'work_complete'}, {})
+
 
 # Direct start of the service as a process with its default name
 factory = ProtocolFactory(EPUWorkerService)
+
+
