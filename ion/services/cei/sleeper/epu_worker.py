79824c498d1d7a91804a8761e8725a7ebe3220d6
Tim Freeman <tim@timfreeman.org>
2010-08-06 14:03:43 -0500

    trying a callLater to avoid the 'op init bug'

--- ion/services/cei/sleeper/epu_worker.py
+++ ion/services/cei/sleeper/epu_worker.py
@@ -2,7 +2,7 @@
 
 import time
 import logging
-from twisted.internet import defer
+from twisted.internet import defer, reactor
 from magnet.spawnable import spawn, Receiver
 from ion.core import bootstrap
 from ion.services.base_service import BaseService
@@ -19,17 +19,23 @@ class EPUWorkerService(BaseService):
     @defer.inlineCallbacks
     def slc_init(self):
         self.queue_name_work = self.get_scoped_name("system", self.spawn_args["queue_name_work"])
-        worker_queue = {self.queue_name_work:{'name_type':'worker'}}
-        yield bootstrap.declare_messaging(worker_queue)
+        self.workReceiver = Receiver(__name__, self.queue_name_work)
+        self.worker_queue = {self.queue_name_work:{'name_type':'worker'}}
+        self.laterinitialized = False
+        reactor.callLater(0, self.later_init)
 
-        workReceiver = Receiver(__name__, self.queue_name_work)
-        self.workReceiver = workReceiver
+    @defer.inlineCallbacks
+    def later_init(self):
+        yield bootstrap.declare_messaging(self.worker_queue)
         self.workReceiver.handle(self.receive)
-        spawnId = yield spawn(workReceiver)
-
+        spawnId = yield spawn(self.workReceiver)
+        logging.debug("spawnId: %s" % spawnId)
+        self.laterinitialized = True
 
     @defer.inlineCallbacks
     def op_work(self, content, headers, msg):
+        if not self.laterinitialized:
+            logger.error("message got here without the later-init")
         logging.info("EPUWorkerService ---doing work--- content:"+str(content))
         time.sleep(content['work_amount'])
         yield self.reply(msg, 'result', {'result':'work_complete'}, {})
