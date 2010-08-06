089ba576298972f709eb347ee840831b255ccfb3
Tim Freeman <tim@timfreeman.org>
2010-08-06 16:28:11 -0500

    side channel to create work on demand

--- ion/services/cei/sleeper/epu_work_producer.py
+++ ion/services/cei/sleeper/epu_work_producer.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python
-
 import logging
 import random
 from twisted.internet import defer
 from twisted.internet.task import LoopingCall
+from twisted.web import server, resource
+from twisted.internet import reactor
 from magnet.spawnable import Receiver
 from ion.services.base_service import BaseService, BaseServiceClient
 from ion.core.base_process import ProtocolFactory
+import Queue
+import uuid
+import ion.services.cei.cei_events as cei_events
 
 logging.basicConfig(level=logging.DEBUG)
 logging.debug('Loaded: '+__name__)
@@ -18,39 +21,65 @@ class EPUWorkProducer(BaseService):
 
     def slc_init(self):
         self.queue_name_work = self.get_scoped_name("system", self.spawn_args["queue_name_work"])
-        self.work_produce_loop = LoopingCall(self.work_produce)
-        self.work_produce_loop.start(1) #XXX temporary for testing
-
-    def op_start_producing(self, content, headers, msg):
-        logging.info("op_start_producing")
-        work_produce_interval = content["work_produce_interval"]
-        self.work_produce_loop.start(work_produce_interval)
+        self.web_resource = Sidechannel()
+        reactor.listenTCP(8000, server.Site(self.web_resource))
+        self.work_produce_loop = LoopingCall(self.work_seek)
+        self.work_produce_loop.start(1, now=False)
 
     @defer.inlineCallbacks
-    def work_produce(self): 
-        work_amount = random.randint(1, 10) # "work", aka sleep for given seconds
-        yield self.send(self.queue_name_work, 'work', {"work_amount":work_amount})
-
-
-class EPUWorkProducerClient(BaseServiceClient):
-    """Client for sending the "start_producing" message to
-    the EPUWorkProducer to start sending work messages to the EPU Workers.
-    """
-
-    def __init__(self, work_produce_interval=1, proc=None, **kwargs):
-        if not 'targetname' in kwargs:
-            kwargs['targetname'] = "epu_work_producer"
-        BaseServiceClient.__init__(self, proc, **kwargs)
-        self.work_produce_interval = work_produce_interval
-
-    @defer.inlineCallbacks
-    def start_work_producer(self):
-        """Send message to epu_work_producer to make it start producing work"""
-        yield self._check_init()
-        logging.debug('Commanding epu_work_producer to start producing')
-        request = {"work_produce_interval":self.work_produce_interval}
-        yield self.send('start_producing', request)
- 
+    def work_seek(self):
+        try:
+            while True:
+                job = self.web_resource.queue.get(block=False)
+                if job == None:
+                    return
+                
+                yield self.send(self.queue_name_work, 'work', {"work_amount":job.length, "batchid":job.batchid, "jobid":job.jobid})
+                
+                # events need dict support first
+                #extra = {"batchid":job.batchid, "jobid":job.jobid, "work_amount":job.length}
+                cei_events.event("workproducer", "JOBSENT", logging)
+                
+        except Queue.Empty:
+            return
 
 # Direct start of the service as a process with its default name
 factory = ProtocolFactory(EPUWorkProducer)
+
+class SleepJob:
+    def __init__(self, jobid, batchid, length):
+        self.jobid = jobid
+        self.batchid = batchid
+        self.length = int(length)
+
+# Sidechannel access to tell service what to do
+class Sidechannel(resource.Resource):
+    isLeaf = True
+    def __init__(self):
+        self.queue = Queue.Queue()
+        
+    def render_GET(self, request):
+        parts = request.postpath
+        if parts[-1] == "":
+            parts = parts[:-1]
+        if len(parts) != 3:
+            request.setResponseCode(500, "expecting three 'args', /batchid/#jobs/#sleepsecs")
+            return
+        
+        try:
+            batchid = parts[0]
+            jobnum = int(parts[1])
+            secperjob = int(parts[2])
+        except Exception,e:
+            request.setResponseCode(500, "expecting three args, /batchid/#jobs/#sleepsecs, those should be INTEGERS..  %s" % e)
+            return
+
+        sleepjobs = []
+        for i in range(jobnum):
+            sleepjobs.append(SleepJob(i, batchid, secperjob))
+        
+        for job in sleepjobs:
+            self.queue.put(job)
+        
+        logging.debug("enqueued %d jobs with %d sleep seconds" % (jobnum, secperjob))
+        return "<html>Success.</html>\n"
