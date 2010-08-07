64d1de294d1283bce7eb717e085aaf8b7ee63b29
Brian Fox <bfox@ucsd.edu>
2010-08-06 17:29:47 -0700

    Changed format of longs to simple hex strings ... eg 0x0123ABC

--- ion/services/coi/hostsensor/encoders.py
+++ ion/services/coi/hostsensor/encoders.py
@@ -9,14 +9,18 @@ import re
 def encodeJSONToXMLRPC(object):
     """
     Encodes a JSON-like object made up of list, dicts, ints, and so
-    on to a compatible XMLRPC representation.  For now, this means
-    converting larger than 32-bit signed integers into a string
-    representation.
+    on to a compatible XMLRPC representation.  For now, the only
+    conversion in this encoder is a larger than 32-bit signed integer
+    to a string representation.  This is to accomodate XMLRPCs
+    lack of 64-bit integer support.
     
     See http://docs.python.org/library/json.html Section 18.2.2
     for the details of a "JSON-like object"
-    
     """
+    
+    # The bulk of the code below is more or less a pass-through
+    # filter for all the JSON types... except for 64-bit integers.
+    
     if hasattr(object, '__iter__'):
         
         # recurse through iterables
@@ -60,12 +64,12 @@ def encodeJSONToXMLRPC(object):
                             ' unsupported type %s = %s'
                             %(type(object), str(object))
                             )
-            
-        # secret sauce - we want to turn large ints to strings    
+        
+        # HERE WE GO, the secret sauce.
+        # We want to turn large integers into strings    
         if isinstance(object,long) or isinstance(object,int):
             if object > 2**31 - 1:
-                return 'long:' + hex(object)
-        
+                return hex(object)
         return object
 
 
@@ -120,7 +124,7 @@ def decodeXMLRPCToJSON(object):
             
         # secret sauce - we want to turn large ints to strings    
         if isinstance(object, str) or isinstance(object, unicode):
-            if re.match("^long\:0x[0-9A-Fa-f]+", object):
+            if re.match("0x[0-9A-Fa-f]+", object):
                 return int(object[5:], 16)
         return object            
 
