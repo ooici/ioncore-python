5e4bfd31f7a4fde51cd62227666c5d7c2f9feaf7
arjuna <arjuna@swamsi.mit.edu>
2010-08-09 11:54:48 -0400

    Implementations

--- ion/services/sa/data_acquisition.py
+++ ion/services/sa/data_acquisition.py
@@ -16,6 +16,10 @@ from ion.core.base_process import ProtocolFactory
 from ion.services.base_service import BaseService, BaseServiceClient
 from ion.data.datastore.datastore_service import DataStoreServiceClient
 
+from ion.resources import sa_resource_descriptions
+from ion.services.sa.instrument_registry import InstrumentRegistryClient
+from ion.services.sa.data_product_registry import DataProductRegistryClient
+
 class DataAcquisitionService(BaseService):
     """Data acquisition service interface
     """
@@ -80,12 +84,70 @@ class DataAcquisitionServiceClient(BaseServiceClient):
         logging.info('Acquire message Service reply: '+str(content))
         defer.returnValue(str(content))
 
-class DataAcquisitionDataStore(DataStoreServiceClient):
-
-	
+class DAInstrumentRegistry(InstrumentRegistryClient):
+    '''
+    Updates the instrument registry.
+    '''
     @defer.inlineCallbacks
-    def test_data_store(self):
-        yield self.push("LongKeyForRepo")        
-        yield self.pull("OtherLongKeyForRepo")
-        yield self.clone("OtherLongKeyForRepo") 
+    def register_instrument(self, userInput):
+        '''
+        Accepts a dictionary containing user inputs and updates the instrument
+        registry.
+        '''    
+        
+        res = sa_resource_descriptions.InstrumentResource.create_new_resource()
+        res = yield self.register_instrument_type(res)
+        
+       
+        
+        ref = res.reference(head=True)
+        
+        res2 = yield self.get_instrument_type(ref)
+        
+        if 'direct_access' in userInput:
+            res2.direct_access = userInput['direct_access']
+            
+        if 'instrumentID' in userInput:
+            res2.instrumentID = userInput['instrumentID']
+        
+        if 'manufacturer' in userInput:
+            res2.manufacturer = userInput['manufacturer']
+            
+        if 'model' in userInput:
+            res2.model = userInput['model']
+            
+        if 'serial_num' in userInput:
+            res2.serial_num = userInput['serial_num']
+            
+        if 'fw_version' in userInput:
+            res2.fw_version = userInput['fw_version']
+            
+        
+        res2 = yield self.register_instrument_type(res2)
+        
+        res3 = yield self.get_instrument_type(ref)
+        
+        defer.returnValue(res3)
+        
+class DADataProductRegistry(DataProductRegistryClient):
+    '''
+    Accepts a dictionary containing user inputs and updates the data product
+    registry.
+    '''    
+    @defer.inlineCallbacks
+    def register_data_product(self, dataProductInput):
+        res = sa_resource_descriptions.DataProductResource.create_new_resource()
+        res = yield self.register_data_product_type(res)
+                
+        ref = res.reference(head=True)
+        
+        res2 = yield self.get_data_product_type(ref)
+        if 'dataformat' in dataProductInput:
+            res2.dataformat = dataProductInput['dataformat']
+        
+        res2 = yield self.register_data_product_type(res2)
+        
+        res3 = yield self.get_data_product_type(ref)
+        
+        defer.returnValue(res3)
     
