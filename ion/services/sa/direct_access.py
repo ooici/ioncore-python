861737da80ab25c8a71ca32758ba86429a44b770
Nalin Pilapitiya <nalin@nalin-laptop.(none)>
2010-08-09 17:01:43 -0400

    Created instrument management, data acquisition, and direct access services

--- ion/services/sa/direct_access.py
+++ ion/services/sa/direct_access.py
@@ -21,72 +21,7 @@ from ion.resources import sa_resource_descriptions
 from ion.services.sa.instrument_registry import InstrumentRegistryClient
 from ion.services.sa.data_product_registry import DataProductRegistryClient
 
-#===============================================================================
-# 
-# class DataAcquisitionService(BaseService):
-#    """Data acquisition service interface
-#    """
-# 
-#    # Declaration of service
-#    declare = BaseService.service_declare(name='data_acquisition', version='0.1.0', dependencies=[])
-# 
-#    def __init__(self, receiver, spawnArgs=None):
-#        # Service class initializer. Basic config, but no yields allowed.
-#        BaseService.__init__(self, receiver, spawnArgs)
-#        logging.info('DataAcquisitionService.__init__()')
-# 
-#    def slc_init(self):
-#        # Service life cycle state. Initialize service here. Can use yields.
-#        pass
-# 
-#    @defer.inlineCallbacks
-#    def op_acquire_block(self, content, headers, msg):
-#        """Service operation: Acquire an entire, fully described version of a
-#        data set.
-#        """
-#	logging.info('op_acquire_block: '+str(content))
-# 
-#        # The following line shows how to reply to a message
-#        yield self.reply_ok(msg, {'value':'op_acquire_block_respond,'+str(content)}, {})
-# 
-#    @defer.inlineCallbacks
-#    def op_acquire_message(self, content, headers, msg):
-#        """Service operation: Acquire an increment of a data set.
-#        """
-#        
-#	logging.info('op_acquire_message: '+str(content))
-# 
-#        # The following line shows how to reply to a message
-#        yield self.reply_ok(msg, {'value':'op_acquire_message_respond, '+str(content)}, {})
-# 
-# # Spawn of the process using the module name
-# factory = ProtocolFactory(DataAcquisitionService)
-# 
-# class DataAcquisitionServiceClient(BaseServiceClient):
-#    """
-#    This is an exemplar service client that calls the Data acquisition service. It
-#    makes service calls RPC style.
-#    """
-#    def __init__(self, proc=None, **kwargs):
-#        if not 'targetname' in kwargs:
-#            kwargs['targetname'] = "data_acquisition"
-#        BaseServiceClient.__init__(self, proc, **kwargs)
-# 
-#    @defer.inlineCallbacks
-#    def acquire_block(self, text='Hi there'):
-#        yield self._check_init()
-#        (content, headers, msg) = yield self.rpc_send('acquire_block', text)
-#	#op_ will be added by default: eval call to run that
-#        logging.info('Acquire block Service reply: '+str(content))
-#        defer.returnValue(str(content))
-# 
-#    @defer.inlineCallbacks
-#    def acquire_message(self, text='Hi there'):
-#        yield self._check_init()
-#        (content, headers, msg) = yield self.rpc_send('acquire_message', text)
-#        logging.info('Acquire message Service reply: '+str(content))
-#        defer.returnValue(str(content))
-#===============================================================================
+
 
 class DAInstrumentRegistry(InstrumentRegistryClient):
     """
--- ion/services/sa/test/test_data_acquisition.py
+++ ion/services/sa/test/test_data_acquisition.py
@@ -8,7 +8,7 @@
 
 from twisted.internet import defer
 
-from ion.services.sa.data_acquisition import DataAcquisitionServiceClient, DAInstrumentRegistry, DADataProductRegistry
+from ion.services.sa.data_acquisition import DAInstrumentRegistry, DADataProductRegistry
 from ion.test.iontest import IonTestCase
 
 from twisted.trial import unittest
@@ -25,13 +25,16 @@ class DataAcquisitionTest(IonTestCase):
 	@defer.inlineCallbacks
 	def setUp(self):
 		yield self._start_container()
-		services = [
-			{'name':'data_acquisition','module':'ion.services.sa.data_acquisition','class':'DataAcquisitionService'},
-		]
-
-		sup = yield self._spawn_processes(services)
-
-		self.da = DataAcquisitionServiceClient(proc=sup)
+		
+#===============================================================================
+#		services = [
+#			{'name':'data_acquisition','module':'ion.services.sa.data_acquisition','class':'DataAcquisitionService'},
+#		]
+# 
+#		sup = yield self._spawn_processes(services)
+# 
+#		self.da = DataAcquisitionServiceClient(proc=sup)
+#===============================================================================
 		
 		services = [
 			{'name':'resourceregistry1','module':'ion.services.sa.instrument_registry','class':'InstrumentRegistryService'}]
@@ -49,17 +52,20 @@ class DataAcquisitionTest(IonTestCase):
 	def tearDown(self):
 		yield self._stop_container()
 
-	@defer.inlineCallbacks
-	def test_acquire_message(self):
-
-		 yield self.da.acquire_message("accessing acquire message")
-
-	@defer.inlineCallbacks
-	def test_acquire_block(self):
-
-		 yield self.da.acquire_block("accessing acquire block")
-
-	
+    
+#===============================================================================
+#	@defer.inlineCallbacks
+#	def test_acquire_message(self):
+# 
+#		 yield self.da.acquire_message("accessing acquire message")
+# 
+#	@defer.inlineCallbacks
+#	def test_acquire_block(self):
+# 
+#		 yield self.da.acquire_block("accessing acquire block")
+#===============================================================================
+		 
+    
 	
 	@defer.inlineCallbacks
 	def test_register_instrument(self):
@@ -87,44 +93,6 @@ class DataAcquisitionTest(IonTestCase):
 		self.assertEqual(Instance.instrument.fw_version, "1")	  
 		self.assertEqual(Instance.dataproduct.dataformat, "binary")
 
-	@defer.inlineCallbacks
-	def test_management_service(self):
+	
 		
-		"""
-		Accepts an dictionary containing updates to the instrument registry.  
-		Updates are made to the registries.
-		""" 
-		
-		userUpdate = {'manufacturer' : "SeaBird Electronics", 
-				 'model' : "unknown model",
-				 'serial_num' : "1234",
-				 'fw_version' : "1"}
-		instrumentID = "500"
-		userUpdate['instrumentID'] = instrumentID
-				
-		Instance.instrument = yield self.IR.register_instrument(userUpdate)
-		
-		dataProductInput = {'dataformat' : "binary"}
-		
-		Instance.dataproduct = yield self.DP.register_data_product(dataProductInput)
-		
-		self.assertEqual(Instance.instrument.instrumentID, "500")
-		self.assertEqual(Instance.instrument.manufacturer, "SeaBird Electronics")
-		self.assertEqual(Instance.instrument.model, "unknown model") #change made
-		self.assertEqual(Instance.instrument.serial_num, "1234")
-		self.assertEqual(Instance.instrument.fw_version, "1")
-		self.assertEqual(Instance.dataproduct.dataformat, "binary")
-		
-	@defer.inlineCallbacks
-	def test_direct_access(self):
-			
-		"""
-		Switches direct_access mode to ON in the instrument registry.
-		"""
-			
-		userUpdate = {'direct_access' : "on"}
-		Instance.instrument = yield self.IR.register_instrument(userUpdate)
-		self.assertEqual(Instance.instrument.direct_access, "on") #change made
-
-		
-
+	
