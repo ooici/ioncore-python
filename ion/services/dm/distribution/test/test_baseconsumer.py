bae9fdd528c1dbf4095e64b25b1aa5d5318ce7dc
Paul Hubbard <pfhubbard@ucsd.edu>
2010-09-07 16:37:57 -0700

    working through dm tests

--- ion/services/dm/distribution/test/test_baseconsumer.py
+++ ion/services/dm/distribution/test/test_baseconsumer.py
@@ -50,12 +50,12 @@ class BaseConsumerTest(IonTestCase):
         queue_properties = {queue2:{'name_type':'fanout', 'args':{'scope':'global'}}}
         yield bootstrap.declare_messaging(queue_properties)
         self.queue2 = queue2
-        
+
         queue3=dataobject.create_unique_identity()
         queue_properties = {queue3:{'name_type':'fanout', 'args':{'scope':'global'}}}
         yield bootstrap.declare_messaging(queue_properties)
         self.queue3 = queue3
-        
+
 
     @defer.inlineCallbacks
     def tearDown(self):
@@ -65,90 +65,90 @@ class BaseConsumerTest(IonTestCase):
 
     @defer.inlineCallbacks
     def test_spawn_attach_args(self):
-        
+
         pd1={'name':'consumer_number_1',
                  'module':'ion.services.dm.distribution.consumers.forwarding_consumer',
                  'procclass':'ForwardingConsumer',
                  'spawnargs':{'attach':[self.queue1]}}
         child1 = base_consumer.ConsumerDesc(**pd1)
-        
+
         child1_id = yield self.test_sup.spawn_child(child1)
-        
+
         # Don't do this - you can only get the instance in a test case -
         # this is not a valid pattern in OTP
         dc1 = self._get_procinstance(child1_id)
-        
+
         self.assertIn(self.queue1,dc1.dataReceivers)
 
 
     @defer.inlineCallbacks
     def test_spawn_attach_msg(self):
-        
+
         pd1={'name':'consumer_number_1', \
                  'module':'ion.services.dm.distribution.consumers.forwarding_consumer',
                  'procclass':'ForwardingConsumer',}
         child1 = base_consumer.ConsumerDesc(**pd1)
-                
+
         child1_id = yield self.test_sup.spawn_child(child1)
-            
+
         res = yield child1.attach(self.queue1)
         self.assertEqual(res,'OK')
         #self.assertEqual(child.proc_attached,self.queue1)
-        
-        
+
+
         res = yield child1.attach(None)
         self.assertEqual(res,'ERROR')
         #self.assertEqual(child.proc_attached,None)
-        
+
         yield child1.shutdown()
 
     @defer.inlineCallbacks
     def test_spawn_attach_inst(self):
-        
+
         pd1={'name':'consumer_number_1', \
                  'module':'ion.services.dm.distribution.consumers.forwarding_consumer',
                  'procclass':'ForwardingConsumer',}
         child1 = base_consumer.ConsumerDesc(**pd1)
-                
+
         child1_id = yield self.test_sup.spawn_child(child1)
-            
+
         # NOT VALID IN OTP TO GET THE INSTANCE ONLY FOR TEST CASE
         dc1 = self._get_procinstance(child1_id)
-            
+
         res = yield dc1.attach(self.queue1)
         #@Todo Assert what?
         self.assert_(res)
         #self.assertEqual(child.proc_attached,self.queue1)
-        
+
         yield dc1.shutdown()
 
 
 
     @defer.inlineCallbacks
     def test_params(self):
-        
+
         pd1={'name':'consumer_number_1', \
                  'module':'ion.services.dm.distribution.consumers.forwarding_consumer',
                  'procclass':'ForwardingConsumer',}
         child1 = base_consumer.ConsumerDesc(**pd1)
-                
+
         child1_id = yield self.test_sup.spawn_child(child1)
-            
+
         # Send a dictionary
         params={'Junk':'Trunk'}
         res = yield child1.set_process_parameters(params)
         self.assertEqual(res,'OK')
 
         res = yield child1.get_process_parameters()
-        
+
         self.assertEqual(res,params)
-        
+
         params2 = {'more junk':'my trunk'}
         res = yield child1.set_process_parameters(params2)
         self.assertEqual(res,'OK')
 
         res = yield child1.get_process_parameters()
-        
+
         ptest = dict(params)
         ptest.update(params2)
         self.assertEqual(res,ptest)
@@ -172,9 +172,9 @@ class BaseConsumerTest(IonTestCase):
         dmsg.notifcation = 'Junk'
         dmsg.timestamp = pu.currenttime()
         dmsg = dmsg.encode()
-        
+
         yield self.test_sup.send(self.queue1, 'data', dmsg)
-        
+
         yield pu.asleep(1)
         msg_cnt = yield child1.get_msg_count()
         received = msg_cnt.get('received',{})
@@ -200,33 +200,33 @@ class BaseConsumerTest(IonTestCase):
         dmsg.notifcation = 'Junk'
         dmsg.timestamp = pu.currenttime()
         dmsg = dmsg.encode()
-        
+
         yield self.test_sup.send(self.queue1, 'data', dmsg)
-        
+
         yield pu.asleep(1)
-        
+
         msg_cnt = yield child1.get_msg_count()
         received = msg_cnt.get('received',{})
         sent = msg_cnt.get('sent',{})
         self.assertEqual(sent.get(self.queue2),1)
         self.assertEqual(received.get(self.queue1),1)
-        
-        
-        #Spawn another process to listen to queue 2  
+
+
+        #Spawn another process to listen to queue 2
         pd2={'name':'consumer_number_2', \
                  'module':'ion.services.dm.distribution.consumers.forwarding_consumer',
                  'procclass':'ForwardingConsumer',\
                  'spawnargs':{'attach':self.queue2}}
-        
+
         child2 = base_consumer.ConsumerDesc(**pd2)
-                
+
         child2_id = yield self.test_sup.spawn_child(child2)
-       
+
         # Tell the first consumer to pass results to the second!
         #res = yield child1.set_params({'queues':[self.queue2]})
-        
+
         yield self.test_sup.send(self.queue1, 'data', dmsg)
-        
+
         yield pu.asleep(1)
 
         msg_cnt = yield child1.get_msg_count()
@@ -234,24 +234,24 @@ class BaseConsumerTest(IonTestCase):
         sent = msg_cnt.get('sent',{})
         self.assertEqual(sent.get(self.queue2),2)
         self.assertEqual(received.get(self.queue1),2)
-        
-        
+
+
         msg_cnt = yield child2.get_msg_count()
         received = msg_cnt.get('received',{})
         sent = msg_cnt.get('sent')
         self.assertEqual(sent,{})
         self.assertEqual(received.get(self.queue2),1)
-        
-        
+
+
         yield child1.shutdown()
         yield child2.shutdown()
-        
-        
-        
-        
+
+
+
+
     @defer.inlineCallbacks
     def test_attach2_send(self):
-        
+
         pd1={'name':'consumer_number_1',
                  'module':'ion.services.dm.distribution.consumers.forwarding_consumer',
                  'procclass':'ForwardingConsumer',
@@ -259,50 +259,53 @@ class BaseConsumerTest(IonTestCase):
                     'process parameters':{},\
                     'delivery queues':{'queues':[self.queue3]}}\
             }
-            
+
         child1 = base_consumer.ConsumerDesc(**pd1)
-        
+
         child1_id = yield self.test_sup.spawn_child(child1)
-        
+
         dc1 = self._get_procinstance(child1_id)
-        
+
         self.assertIn(self.queue1,dc1.dataReceivers)
         self.assertIn(self.queue2,dc1.dataReceivers)
-        
+
         dmsg = DataMessageObject()
         dmsg.notifcation = 'Junk'
         dmsg.timestamp = pu.currenttime()
         dmsg = dmsg.encode()
-        
+
         # Send to queue1
         yield self.test_sup.send(self.queue1, 'data', dmsg)
-        
+
         yield pu.asleep(1)
-        
+
         msg_cnt = yield child1.get_msg_count()
         received = msg_cnt.get('received',{})
         sent = msg_cnt.get('sent',{})
         self.assertEqual(sent.get(self.queue3),1)
         self.assertEqual(received.get(self.queue1),1)
-        
+
         # Send to queue2
         yield self.test_sup.send(self.queue2, 'data', dmsg)
-        
+
         yield pu.asleep(1)
-        
+
         msg_cnt = yield child1.get_msg_count()
         received = msg_cnt.get('received',{})
         sent = msg_cnt.get('sent',{})
         self.assertEqual(sent.get(self.queue3),2)
         self.assertEqual(received.get(self.queue1),1)
         self.assertEqual(received.get(self.queue2),1)
-        
+
         yield child1.shutdown()
-        
-        
-        
+
+
+
     @defer.inlineCallbacks
     def test_deattach(self):
+
+        raise unittest.SkipTest("Magnet does not yet support deattach.")
+
         pd1={'name':'consumer_number_1',
                  'module':'ion.services.dm.distribution.consumers.forwarding_consumer',
                  'procclass':'ForwardingConsumer',
@@ -318,29 +321,27 @@ class BaseConsumerTest(IonTestCase):
         dmsg.notifcation = 'Junk'
         dmsg.timestamp = pu.currenttime()
         dmsg = dmsg.encode()
-        
+
         yield self.test_sup.send(self.queue1, 'data', dmsg)
-        
+
         msg_cnt = yield child1.get_msg_count()
         received = msg_cnt.get('received',{})
         sent = msg_cnt.get('sent',{})
         self.assertEqual(sent.get(self.queue2),1)
         self.assertEqual(received.get(self.queue1),1)
-        
-        raise unittest.SkipTest("Magnet does not yet support deattach.")
-        
+
         res = yield child1.deattach(self.queue1)
         self.assertEqual(res,'OK')
-        
+
         yield self.test_sup.send(self.queue1, 'data', dmsg)
-        
+
         msg_cnt = yield child1.get_msg_count()
         received = msg_cnt.get('received',{})
         sent = msg_cnt.get('sent',{})
         self.assertEqual(sent.get(self.queue2),1)
         self.assertEqual(received.get(self.queue1),1)
-        
-        
+
+
     @defer.inlineCallbacks
     def test_digest_latest(self):
         pd1={'name':'consumer_number_1',
@@ -359,25 +360,19 @@ class BaseConsumerTest(IonTestCase):
         dmsg.notifcation = 'Junk'
         dmsg.timestamp = pu.currenttime()
         dmsg = dmsg.encode()
-        
+
         yield self.test_sup.send(self.queue1, 'data', dmsg)
-        
+
         yield self.test_sup.send(self.queue1, 'data', dmsg)
-        
+
         yield self.test_sup.send(self.queue1, 'data', dmsg)
-        
+
         yield self.test_sup.send(self.queue1, 'data', dmsg)
-        
-        yield pu.asleep(2)
-        
+
+        yield pu.asleep(3)
+
         msg_cnt = yield child1.get_msg_count()
         received = msg_cnt.get('received',{})
         sent = msg_cnt.get('sent',{})
         self.assertEqual(sent.get(self.queue2),1)
         self.assertEqual(received.get(self.queue1),4)
-        
-        
-        
-        
-
-        
