5a5bea962d7f63c25e59157fc92565b2a5461155
Michael Meisinger <mmeisinger@ucsd.edu>
2010-09-20 20:25:29 -0700

    Removed AMQP specs, not needed because in carrot

--- ion/core/messaging/__init__.py
+++ ion/core/messaging/__init__.py
@@ -1,4 +1,4 @@
-import os
+#import os
 
 # Spec file is loaded from the egg bundle. Hard code 0-8 for now...
-spec_path_def = os.path.join('res', 'spec', 'amqp0-8.xml')
+#spec_path_def = os.path.join('res', 'spec', 'amqp0-8.xml')
