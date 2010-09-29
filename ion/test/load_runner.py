a9c3fac3c6ec1a23cd898bd910d7908ddf20b5e4
Michael Meisinger <mmeisinger@ucsd.edu>
2010-09-28 17:03:45 -0700

    Enhanced load test framework and broker connect test

--- ion/test/load_runner.py
+++ ion/test/load_runner.py
@@ -6,8 +6,8 @@
 @brief Spawns a number of Unix processes to create some load
 """
 
-#import ion.util.ionlog
-#log = ion.util.ionlog.getLogger(__name__)
+import ion.util.ionlog
+log = ion.util.ionlog.getLogger(__name__)
 
 import subprocess
 import sys
@@ -32,39 +32,49 @@ class Options(usage.Options):
                 ["class", "c", None, "Qualified load test class"],
                 ["count", "n", 1, "Number of load processes"],
                 ["timeout", "t", 0, "Seconds after which to kill the load processes"],
+                ["loadid", "l", 0, "Identity (0-based number) of load process"],
                     ]
     optFlags = [
-                ["suite", "s", "Run test suite"],
-                ["proc", "p", "Spawn sub-processes for individual tests"],
+                ["suite", "s", "Run load test suite"],
+                ["proc", "p", "Spawn individual load processes"],
                 ]
 
     def parseArgs(self, *args):
         self['test_args'] = args
+        #print "Extra args", args
 
     def opt_version(self):
         from ion.core.ionconst import VERSION
-        log.info("ION  version: "+ VERSION)
+        print "ION version: ", VERSION
         sys.exit(0)
 
 class LoadTestRunner(object):
 
+    def __init__(self):
+        self.exit_level = 0
+
     @defer.inlineCallbacks
     def start_load_suite(self, suitecls, spawn_procs, options):
-        print "Start load suite %s as procs:%s" % (suitecls, spawn_procs)
+        print "Start load suite %s" % (suitecls)
         numprocs = int(options['count'])
 
         if spawn_procs:
+            # Start all as separate unix processes
             load_script = ['python', '-m', 'ion.test.load_runner', '-c', options['class']]
             timeout = int(options['timeout'])
-            if timeout > 10:
-                load_script.extend(['-t',str(timeout-10)])
+            if timeout > 0:
+                load_script.extend(['-t', str(timeout)])
 
-            load_script.extend(options['test_args'])
-            print "Starting %s loads: %s" % (numprocs, load_script)
+            print "Spawning %s load processes: %s" % (numprocs, options['class'])
 
             procs = []
             for i in range(numprocs):
-                p = subprocess.Popen(load_script)
+                load_proc_args = list(load_script)
+                load_proc_args.extend(["-l", str(i)])
+                load_proc_args.extend(options['test_args'])
+                #print "Starting load process %s: %s" % (i, load_proc_args)
+
+                p = subprocess.Popen(load_proc_args)
                 procs.append(p)
 
             #print "started all procs"
@@ -73,56 +83,94 @@ class LoadTestRunner(object):
                 p.wait()
 
         else:
+            # Start all in same Twisted reactor
             deflist = []
             for i in range(numprocs):
-                load_proc = suitecls()
-                #defer.maybeDeferred(load_proc.setUp)
-                d = defer.maybeDeferred(load_proc.generate_load)
+                d = self.start_load_proc(suitecls, str(i), options)
                 deflist.append(d)
 
             dl = defer.DeferredList(deflist)
             yield dl
 
     @defer.inlineCallbacks
-    def start_load_proc(self, suitecls, options):
+    def start_load_proc(self, suitecls, loadid, options):
+        """
+        Starts a single load in the current processes
+        @retval Deferred
+        """
         load_proc = suitecls()
+        load_proc.load_id = str(loadid)
+        load_proc.options = options
+
         yield defer.maybeDeferred(load_proc.setUp)
         yield defer.maybeDeferred(load_proc.generate_load)
-        yield defer.maybeDeferred(load_proc.tearDown)
+
+        #d3 = defer.maybeDeferred(load_proc.tearDown)
 
     def timeout(self):
-        print "TIMEOUT"
+        #print "TIMEOUT"
+        if reactor.running:
+            try:
+                reactor.stop()
+            except Exception, ex:
+                pass
+
+    @defer.inlineCallbacks
+    def load_runner_main(self):
+        try:
+            # Parse the options
+            options = Options()
+            try:
+                options.parseOptions(sys.argv[1:])
+            except usage.UsageError, errortext:
+                print '%s: %s' % (sys.argv[0], errortext)
+                print '%s: Try --help for usage details.' % (sys.argv[0])
+                sys.exit(1)
+
+            if not options['class']:
+                sys.exit(0)
+
+            try:
+                test_class = pu.get_class(options['class'])
+            except Exception, ex:
+                self.errout(str(ex))
+
+            if not issubclass(test_class, LoadTest):
+                self.errout("Class must be LoadTest")
+
+            timeout = int(options['timeout'])
+            if timeout > 0:
+                self.timeout_call = reactor.callLater(timeout, self.timeout)
+
+            if options['suite']:
+                yield self.start_load_suite(test_class, options['proc'], options)
+                print "Load test suite stopped."
+            elif options['loadid']:
+                yield self.start_load_proc(test_class, options['loadid'], options)
+            else:
+                self.errout("Wrong arguments: Neither suite nor load process")
+
+            try:
+                self.timeout_call.cancel()
+            except Exception, ex:
+                pass
+
+        except SystemExit, se:
+            self.exit_level = se.code
+
         if reactor.running:
             reactor.stop()
 
-    @defer.inlineCallbacks
-    def start_load_runner(self):
-        options = Options()
-        try:
-            options.parseOptions(sys.argv[1:])
-        except usage.UsageError, errortext:
-            print '%s: %s' % (sys.argv[0], errortext)
-            print '%s: Try --help for usage details.' % (sys.argv[0])
-            sys.exit(1)
 
-        test_class = pu.get_class(options['class'])
-        #print "Load test class", test_class
-        assert issubclass(test_class, LoadTest), "Class must be LoadTest"
-
-        timeout = int(options['timeout'])
-        if timeout > 0:
-            reactor.callLater(timeout, self.timeout)
-
-        if options['suite']:
-            yield self.start_load_suite(test_class, options['proc'], options)
-        else:
-            yield self.start_load_proc(test_class, options)
-
-        reactor.stop()
+    def errout(self, message):
+        print '%s: %s' % (sys.argv[0], message)
+        sys.exit(1)
 
 if __name__ == '__main__':
     testrunner = LoadTestRunner()
-    reactor.callWhenRunning(testrunner.start_load_runner)
+    reactor.callWhenRunning(testrunner.load_runner_main)
     #print "Starting reactor"
-    reactor.run( )
+    r = reactor.run()
     #print "Reactor stopped"
+    if testrunner.exit_level > 0:
+        sys.exit(testrunner.exit_level)
--- ion/test/loadtests/brokerload.py
+++ ion/test/loadtests/brokerload.py
@@ -8,15 +8,82 @@
 
 from twisted.internet import defer
 
-from ion.test.loadtest import LoadTest
+from carrot import connection, messaging
+
+import ion.util.ionlog
+log = ion.util.ionlog.getLogger(__name__)
+
+from ion.test.loadtest import LoadTest, LoadTestOptions
 import ion.util.procutils as pu
 
-#import ion.util.ionlog
-#log = ion.util.ionlog.getLogger(__name__)
+class BrokerTestOptions(LoadTestOptions):
+    optParameters = [
+                ["scenario", "s", "connect", "Load test scenario"],
+                ["host", "h", "localhost", "Broker host name"],
+                ["port", "p", 5672, "Broker port"],
+                ["vhost", "v", "/", "Broker vhost"],
+                ["heartbeat", None, 0, "Heartbeat rate [seconds]"],
+                    ]
+    optFlags = [
+                ]
+
 
 class BrokerTest(LoadTest):
 
+    def setUp(self):
+        numopt = len(self.options['test_args'])
+        assert numopt >= 2
+        self.scenario = self.options['test_args'][0]
+        self.broker_host = self.options['test_args'][1]
+        self.broker_port = numopt >= 3 and self.options['test_args'][2] or 5672
+        self.broker_vhost = numopt >= 4 and self.options['test_args'][3] or "/"
+
+        self.cur_state['connects'] = 0
+        self.cur_state['msgsend'] = 0
+        self.cur_state['msgrecv'] = 0
+        self.cur_state['errors'] = 0
+
+        self._enable_monitor(5)
+
     @defer.inlineCallbacks
-    def generate_load(self, *args):
-        yield pu.asleep(5)
-        print "LOAD"
+    def generate_load(self):
+        if self.scenario == "connect":
+            while True:
+                #print "%s: connect to %s:%s" % (self.load_id, self.broker_host, self.broker_port)
+
+                self.connection = connection.BrokerConnection(
+                                        hostname=self.broker_host,
+                                        port=self.broker_port,
+                                        virtual_host=self.broker_vhost,
+                                        heartbeat=0)
+
+                yield self.connection.connect()
+                #print "%s: connected" % (self.load_id)
+
+                #yield pu.asleep(0.1)
+
+                yield self.connection._connection.transport.loseConnection()
+
+                #print "%s: disconnected" % (self.load_id)
+
+                self.cur_state['connects'] += 1
+
+                #yield pu.asleep(1)
+
+    def tearDown(self):
+        self.monitor()
+
+    def monitor(self):
+        interval = self._get_interval()
+
+        rates = self._get_state_rate()
+        print "%s: new state  %s" % (self.load_id, self.cur_state)
+        print "%s: rate state %s" % (self.load_id, rates)
+
+        #print "%s: performed %s connect (rate %s), %s send, %s receive, %s error" % (
+        #    self.load_id, self.connects, connect_rate, self.msgsend, self.msgrecv, self.errors)
+
+
+"""
+python -m ion.test.load_runner -s -c ion.test.loadtests.brokerload.BrokerTest -p connect
+"""
