4e5a97d167b90339dde64a4932f4881ba710d6ff
Dave Foster <dfoster@asascience.com>
2010-11-30 10:01:08 -0500

    Fixed termios, again

--- ion/core/cc/shell.py
+++ ion/core/cc/shell.py
@@ -4,7 +4,7 @@
 @brief Python Capability Container shell
 """
 
-import os, sys, tty, termios, platform
+import os, sys, tty, termios
 
 from twisted.internet import stdio
 from twisted.conch.insults import insults
@@ -133,11 +133,10 @@ class Control(object):
         tty.setraw(fd, termios.TCSANOW) # when=now
         self.fd = fd
 
-        # stdout fd (only adjust on OSX)
-        if platform.system() == 'Darwin':
-            outSettings = termios.tcgetattr(fdout)
-            outSettings[1] = 3 # do CR
-            termios.tcsetattr(fdout, termios.TCSANOW, outSettings)
+        # stdout fd
+        outSettings = termios.tcgetattr(fdout)
+        outSettings[1] = termios.OPOST | termios.ONLCR
+        termios.tcsetattr(fdout, termios.TCSANOW, outSettings)
 
         namespace = makeNamespace()
 
