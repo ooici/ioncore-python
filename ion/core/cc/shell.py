e9d0cba7a28922f73a68251376221e1a75d3c001
Dorian Raymer <deldotdr@gmail.com>
2011-01-04 15:49:45 -0800

    Remove using log from shell.py
    Used os.write to print the 'press ctrl-c' message on shell exit.

--- ion/core/cc/shell.py
+++ ion/core/cc/shell.py
@@ -19,9 +19,6 @@ from twisted.conch.insults import insults
 from twisted.conch import manhole, recvline
 from twisted.python import text
 
-import ion.util.ionlog
-log = ion.util.ionlog.getLogger(__name__)
-
 from ion.core import ionconst
 
 CTRL_A = '\x01'
@@ -588,7 +585,6 @@ class Control(object):
     pre_namespace = {}
 
     def start(self, ccService):
-        #log.info('Shell Start')
         fd = sys.__stdin__.fileno()
         fdout = sys.__stdout__.fileno()
 
@@ -621,10 +617,9 @@ class Control(object):
 
     def stop(self):
         termios.tcsetattr(self.fd, termios.TCSANOW, self.oldSettings)
-        #log.info('Shell Stop')
         # if terminal write reset doesnt work in handle QUIT, use this
         os.write(self.fd, "\r\x1bc\r")
-        log.info('Shell exited. Press Ctrl-c to stop container')
+        os.write(self.fd, 'Shell exited. Press Ctrl-c to stop container\n')
 
     def add_term_name(self, key, value):
         if self.namespace:
