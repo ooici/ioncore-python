e1cbc967c1835745f3cc287988a5050b673adf64
David Stuebe <dstuebe@asascience.com>
2011-01-22 10:23:08 -0500

    Fixed up the hello_object example

--- ion/play/hello_object.py
+++ ion/play/hello_object.py
@@ -12,83 +12,102 @@ from twisted.internet import defer
 
 import ion.util.procutils as pu
 from ion.core.process.process import ProcessFactory, Process, ProcessClient
-#from ion.core.process.service_process import ServiceProcess, ServiceClient
+from ion.core.process.service_process import ServiceProcess, ServiceClient
 
 from ion.core.object import object_utils
 
-class HelloObject(Process):
+addresslink_type = object_utils.create_type_identifier(object_id=20003, version=1)
+person_type = object_utils.create_type_identifier(object_id=20001, version=1)
+
+class HelloError(Exception):
+    """
+    An exception class for the Hello Object example
+    """
+
+
+class HelloObject(ServiceProcess):
     """
     Example process sends objects
     """
+
+    # Declaration of service
+    declare = ServiceProcess.service_declare(name='hello_object',
+                                             version='0.1.0',
+                                             dependencies=[])
 
     def slc_init(self):
         # Service life cycle state. Initialize service here. Can use yields.
         log.info('SLC_INIT HelloProcess')
 
     @defer.inlineCallbacks
-    def op_hello(self, content, headers, msg):
-        log.info('op_hello: ')
+    def op_hello_person(self, person, headers, msg):
+        log.info('op_hello_person: ')
 
-        ab = content
+        # Check only the type recieved and linked object types. All fields are
+        #strongly typed in google protocol buffers!
+        if person.GPBType != person_type:
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloError('Unexpected type received %s; type: %s' % (str(person), str(person.GPBType)))
+            
                 
-        p = ab.person[0]
+        # Creepy hello person object log statements...
+        log.info( 'Hello ' + person.name +'...')
+        log.info('I know your phone number ' + person.name + '... it is: '+ person.phone[0].number)
         
-        assert p.name == 'david', 'Not reading right!'
+        # Change something...
         
-        print 'Print Test'
-        for person in ab.person:
-            print 'person',person
-        print 'owner',ab.owner
+        person.name = person.name + ' stuebe'
         
-        
-        
-        ab.person.add()
-        p = ab.repository.create_wrapped_object(addressbook_pb2.Person)
-        p.name = 'John'
-        p.id = 109
-        p.email = 'john@doe.com'
-        
-        ab.person[1] = p
-        
-        ab.owner = p
-        
-        print 'AB', ab
-        
-        print 'Commiting.....'
-        
-        ab.repository.commit('adding a commit')
-        
-        print 'Commited, now sending reply'
+        # Print the name of the repository which holds this object
+        log.info('The name of the person object in the service: ' +person.Repository.repository_key)
         
         # The following line shows how to reply to a message
-        yield self.reply(msg, 'result', ab)
-
-
-class HelloObjectClient(ProcessClient):
-    """
-    This is an exemplar process client that calls the hello process. It
-    makes service calls RPC style.
-    """
+        yield self.reply_ok(msg, person)
 
     @defer.inlineCallbacks
-    def hello(self, msg):
+    def op_hello_everyone(self, addressbook, headers, msg):
+        log.info('op_hello_everyone: ')
+
+        if addressbook.GPBType != addresslink_type:
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloError('Unexpected type received %s; type: %s' % (str(addressbook), str(addressbook.GPBType)))
+            
+        log.info('Received addresbook; Title: ' + addressbook.title)
+        for person in addressbook.person:
+            log.info('Logging Person: \n' +str(person))
+            
+        yield self.reply_ok(msg)
+
+
+class HelloObjectClient(ServiceClient):
+    """
+    This is an exemplar service client that calls the hello object service. It
+    makes service calls RPC style using GPB object. There is no special handling
+    here, just call send. The clients should become exteremly thin wrappers with
+    no business logic.
+    """
+
+    def __init__(self, proc=None, **kwargs):
+        if not 'targetname' in kwargs:
+            kwargs['targetname'] = "hello_object"
+        ServiceClient.__init__(self, proc, **kwargs)
+
+    @defer.inlineCallbacks
+    def hello_person(self, msg):
         yield self._check_init()
         
-        
-        
-        (content, headers, msg) = yield self.rpc_send('hello', ab)
-        log.info('Process replied: ')
-        
-        for key,item in content.repository._workspace.items():
-            print 'WORKSPACE,',key,item
-        
-        for person in content.person:
-            print 'person',person
-        print 'owner',content.owner
-        
-        #print 'LinkClassType',ab.LinkClassType
+        (content, headers, msg) = yield self.rpc_send('hello_person', msg)
         
         defer.returnValue(content)
+        
+    @defer.inlineCallbacks
+    def hello_everyone(self, msg):
+        yield self._check_init()
+        
+        (content, headers, msg) = yield self.rpc_send('hello_everyone', msg)
+        
+        defer.returnValue(content)
+
 
 # Spawn of the process using the module name
 factory = ProcessFactory(HelloObject)
--- ion/play/test/test_hello_object.py
+++ ion/play/test/test_hello_object.py
@@ -24,7 +24,10 @@ person_type = object_utils.create_type_identifier(object_id=20001, version=1)
 
 class HelloProcessTest(IonTestCase):
     """
-    Testing example hello service.
+    Testing example hello object service.
+    This example shows how it is possible to create and send strongly typed objects
+    Each time an object is sent it is assigned a new identifier. The example
+    shows how it is possible to move a linked composite from one object to another.
     """
 
     @defer.inlineCallbacks
@@ -43,34 +46,73 @@ class HelloProcessTest(IonTestCase):
             
         proc1 = ProcessDesc(**pd1)
             
+        # Spawn our test process
         proc1_id = yield self.test_sup.spawn_child(proc1)
             
-        # Each 
+        # Each process has a object work bench. In the test we can get a work bench
+        # from the test supervisor process.
         repo, ab = self.test_sup.workbench.init_repository(addresslink_type)
         
+        ab.title = 'An addressbook object for testing'
+        
+        # Add a new entry in the list (repeated) persons of the addressbook
         ab.person.add()
     
-        p = repo.create_object(person_type)
-        p.name = 'david'
-        p.id = 59
-        p.email = 'stringgggg'
-        ab.person[0] = p
+        # Make a new person object to go in the list
+        person = repo.create_object(person_type)
+        person.name = 'david'
+        person.id = 59
+        person.email = 'stringgggg'
+        ph = person.phone.add()
+        ph.number = '401 789 6224'
         
-        print 'AdressBook!',ab
-        repo.commit('My addresbook test')
+        # Since ab.person is of type 'link' we need to set the link equal to the
+        # person object
+        ab.person[0] = person
+        
+        log.info('AdressBook!' + str(ab))
+        # Calling commit is optional - sending will automatically commit...
+        #repo.commit('My addresbook test')
+        
+        log.info('Repository Status: ' + str(repo.status))
+        
+        # Create a hello object client
+        hc1 = HelloObjectClient(proc=self.test_sup)
+        
+        log.info('Calling hello person...')
+        # You can send the root of the object or any linked composite part of it.
+        new_person = yield hc1.hello_person(ab.person[0])
+        
+        # This is a different repository from the one we started with!
+        log.info('Nome of the command object:' + person.Repository.repository_key)
+        log.info('Nome of the response object:' + new_person.Repository.repository_key)
 
-
-
-    #    
-    #    
-    #    log.info('Calling hello there with hc(sup2)')
-    #    hc1 = HelloObjectClient(proc=sup2,target=proc1_id)
-    #    yield hc1.hello("Hi there, hello1")
-    #
-    #
-    #    #log.info('Calling hello there with hc(sup2)')
-    #    #hc2 = HelloObjectClient(proc=sup2,target=proc1_id)
-    #    #yield hc2.hello("Hi there, hello1")
-    #
-    #
-    #    log.info('Tada!')
+        log.info('Repository Status: ' + str(repo.status))
+        
+        
+        # Now lets add the new_person from the reply to the addressbook        
+        p2 = ab.person.add()
+        
+        # You can move a linked object from one repository to another when it
+        # has been commited. An exception will be raised if you try and move
+        # a modified object. Only a linked composite can be moved
+        #p2.SetLink(new_person)
+        # you can use an assignment operator or the set link command...
+        ab.person[1] = new_person
+        
+        # The address book now has two perosn object, but you only see the links in the log
+        log.info('Print the addressbook after adding the respoonse: ' + str(ab))
+        log.info('The hash name of the original person: ' + person.MyId)
+        log.info('The hash name of the new_person: ' + new_person.MyId)
+        
+        
+        # Lets try sending the addressbook now...
+        # Rather than ssending just one of its person objects, send the whole thing
+        yield hc1.hello_everyone(ab)
+        
+        
+        log.info('Tada!')
+        
+        
+        
+        
