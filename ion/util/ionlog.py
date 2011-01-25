c3fc006f973282d596ab3f353f126281c1ec0948
Dave Foster <dfoster@asascience.com>
2011-01-25 16:03:35 -0500

    getLogger now uses a global log factory, can register additional log handlers with

--- ion/util/ionlog.py
+++ ion/util/ionlog.py
@@ -6,36 +6,86 @@
 @brief Abstracts from any form of logging in ION
 """
 
+class LogFactory(object):
+    """
+    Factory for producing logger objects with additional handlers.
+    A global instance of this factory is declared in this module, and
+    is used by the getLogger global used all over ioncore-python.
+    """
+    def __init__(self):
+        """
+        Initializer.
+        """
+        self._handlers = []
+
+    def get_logger(self, loggername):
+        """
+        Creates an instance of a logger.
+        Adds any registered handlers with this factory.
+
+        Note: as this method is called typically on module load, if you haven't
+        registered a handler at this time, that instance of a logger will not
+        have that handler.
+        """
+        logger = logging.getLogger(loggername)
+        for handler in self._handlers:
+            logger.addHandler(handler)
+
+        return logger
+
+    def add_handler(self, handler):
+        """
+        Adds a handler to be added to the logger requested with get_logger.
+        The handler must be derived from logging.Handler.
+        """
+        self._handlers.append(handler)
+
+    def remove_handler(self, handler):
+        """
+        Removes a handler.
+        """
+        self._handlers.remove(handler)
+
+# declare global instance
+try:
+    log_factory
+except NameError:
+    log_factory = LogFactory()
+
 def getLogger(loggername=__name__):
     """
     This function is used to assign every module in the code base a separate
     logger instance. Currently it just delegates to Python logging.
     """
-    import logging
-    return logging.getLogger(loggername)
+    return log_factory.get_logger(loggername)
 
-def IonLogger(object):
-    import logging
+import logging
+class IonLogger(object):
+    """
+    Simple wrapper around a python logger.
+
+    Currently unused.
+    """
     def __init__(self, loggername):
         self.logger = logging.getLogger(loggername)
 
     def debug(self, msg, *args):
-        pass
+        self.logger.debug(msg, *args)
 
     def info(self, msg, *args):
-        pass
+        self.logger.info(msg, *args)
 
     def warn(self, msg, *args):
-        pass
+        self.logger.warn(msg, *args)
 
     def error(self, msg, *args):
-        pass
+        self.logger.error(msg, *args)
 
     def critical(self, msg, *args):
-        pass
+        self.logger.critical(msg, *args)
 
     def exception(self, msg, *args):
-        pass
+        self.logger.exception(msg, *args)
 
     warning = warn
 
