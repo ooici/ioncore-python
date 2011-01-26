9e26e00e93251a8faa99397ebb5cf94dd4551228
David Stuebe <dstuebe@asascience.com>
2011-01-26 18:34:01 -0500

    Added resource examples..

---
+++ ion/play/hello_resource.py
@@ -0,0 +1,339 @@
+#!/usr/bin/env python
+
+"""
+@file ion/play/hello_resource.py
+@author David Stuebe
+@brief An example service definition that can be used as template for resource management.
+"""
+
+import ion.util.ionlog
+log = ion.util.ionlog.getLogger(__name__)
+from twisted.internet import defer
+
+import ion.util.procutils as pu
+from ion.core.process.process import ProcessFactory, Process, ProcessClient
+from ion.core.process.service_process import ServiceProcess, ServiceClient
+
+from ion.core.messaging.message_client import MessageClient
+
+
+from ion.services.coi.resource_registry_beta.resource_client import ResourceClient, ResourceInstance
+from ion.services.coi.resource_registry_beta.resource_client import ResourceClientError, ResourceInstanceError
+
+from ion.core.object import object_utils
+
+instrument_resource_type = object_utils.create_type_identifier(object_id=20029, version=1)
+instrument_info_type = object_utils.create_type_identifier(object_id=20030, version=1)
+resource_request_type = object_utils.create_type_identifier(object_id=10, version=1)
+resource_response_type = object_utils.create_type_identifier(object_id=12, version=1)
+
+resource_reference_type = object_utils.create_type_identifier(object_id=4, version=1)
+
+
+class HelloResourceError(Exception):
+    """
+    An exception class for the Hello Resource example
+    """
+
+
+class HelloResource(ServiceProcess):
+    """
+    Example service which manages a set of resources.
+    
+    It is possible to make fewer "op_'s" and send more complex arguments.
+    Here I have choosen to break out the various operations to demonstrate each
+    function more clearly. It is up to the developer and the architecture team
+    whether there one 'update' operation and the mode (clobber, merge, append) is
+    sent as part of the message object, or like in this example, there are
+    multiple operations one for each mode.
+    
+    Similarly this example shows an service which 'loosly' controls its resources.
+    The caller is has access to the resources by reference. With a resource client
+    the caller could just as easily make changes directly.
+    
+    The developer may wish to abstract the calls to the service and never expose
+    the actual resource references to the caller.
+    """
+
+    # Declaration of service
+    declare = ServiceProcess.service_declare(name='hello_resource',
+                                             version='0.1.0',
+                                             dependencies=[])
+
+    def slc_init(self):
+        # Service life cycle state. Initialize service here. Can use yields.
+        
+        # Can be called in __init__ or in slc_init... no yield required
+        self.rc = ResourceClient(proc=self)
+        
+        self.mc = MessageClient(proc=self)
+        
+        self.instance_counter = 1
+        
+        log.info('SLC_INIT HelloResource')
+
+    @defer.inlineCallbacks
+    def op_create_instrument_resource(self, request, headers, msg):
+        """
+        This method assumes that the caller provides an Instrument Info Object
+        in a Resource Configuration Request message which should be made into a
+        resource.
+        
+        This is the standard pattern for working with a resource.
+        
+        
+        """
+        log.info('op_create_instrument_resource: ')
+
+        # Check only the type recieved and linked object types. All fields are
+        #strongly typed in google protocol buffers!
+        if request.MessageType != resource_request_type:
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloDataUpdateError('Expected message type ResourceConfigurationRequest, received %s'
+                                     % str(request))
+            
+        ### Check the type of the configuration request
+        if request.HasField('resource_reference'):
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloResourceError('Expected message with NO resource_reference field, received an illegal message!')
+            
+        # Attributes of a resource like name and description should be controlled
+        # by the service that manages them
+        name = 'Insturment #%d' % self.instance_counter
+        self.instance_counter+=1
+        
+        # Use the resource client to create a resource!
+        resource = yield self.rc.create_instance(instrument_resource_type, name=name, description='Preposterous instrument resource!')
+        
+        ### Set any fields provided by the configuration request
+        if request.HasField('configuration'):
+            
+            ### Check the type of the configuration request
+            if request.configuration.ObjectType != instrument_info_type:
+                # This will terminate the hello service. As an alternative reply okay with an error message
+                raise HelloResourceError('Expected message type InstrumentInfoRequest, received %s; type %s'
+                                         % (str(request.configuration), str(request.configuration.ObjectClass)))
+                
+            if request.configuration.HasField('name'):
+                resource.name = request.configuration.name
+                
+            if request.configuration.HasField('make'):
+                resource.make = request.configuration.make
+                
+            if request.configuration.HasField('model'):
+                resource.model = request.configuration.model
+                
+            if request.configuration.HasField('serial_number'):
+                resource.serial_number = request.configuration.serial_number
+        
+        
+        yield self.rc.put_instance(resource)
+        
+        log.info(str(resource))
+        
+        
+        response = yield self.mc.create_instance(resource_response_type, name='create_instrument_resource response')
+        
+        # Create a reference to return to the caller
+        # This is one pattern - it exposes the resource to the caller
+        
+        # pass the reference
+        response.resource_reference = self.rc.reference_instance(resource)
+        
+        # pass the current configuration
+        response.configuration = resource.ResourceObject
+        
+        # pass the result of the create operation...
+        response.result = 'Created'
+                
+        # The following line shows how to reply to a message
+        yield self.reply_ok(msg, response)
+
+
+    @defer.inlineCallbacks
+    def op_update_instrument_resource(self, request, headers, msg):
+        """
+        This method is an example using a standard resource update request.
+        It contains two parts, the reference to the resource to update and the
+        state of the resource which should be set.
+        """
+        
+        log.info('op_update_instrument_resource: ')
+            
+        # Check only the type recieved and linked object types. All fields are
+        #strongly typed in google protocol buffers!
+        if request.MessageType != resource_request_type:
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloResourceError('Expected message type ResourceConfigurationRequest, received %s'
+                                     % str(request))
+        
+        ### Check the type of the configuration request
+        if not request.HasField('configuration'):
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloResourceError('Expected message with configuration field of type InstrumentInfoRequest, received empty configuration!')
+
+        if request.configuration.ObjectType != instrument_info_type:
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloResourceError('Expected message with configuration field of type InstrumentInfoRequest, received %s; type %s'
+                    % (str(request.configuration), str(request.configuration.ObjectClass)))
+
+        ### Check the type of the configuration request
+        if not request.HasField('resource_reference'):
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloResourceError('Expected message with resource_reference field of type IDRef, received empty resource_reference!')
+            
+        if request.resource_reference.ObjectType != resource_reference_type:
+
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloResourceError('Expected message with resource_reference field of type IDRef, received %s; type %s'
+                    % (str(request.resource_reference), str(request.resource_reference.ObjectClass)))
+
+
+            
+        # Get the current state of the resource
+        resource = yield self.rc.get_instance(request.resource_reference)
+        
+        ### Set any fields provided by the configuration request
+        if request.HasField('configuration'):
+
+            ### Check the type of the configuration request
+            
+            # This method will set the field in the resource if it is set in the message.
+            #if request.configuration.HasField('name'):
+            #    resource.name = request.configuration.name
+            #    
+            #if request.configuration.HasField('make'):
+            #    resource.make = request.configuration.make
+            #    
+            #if request.configuration.HasField('model'):
+            #    resource.model = request.configuration.model
+            #    
+            #if request.configuration.HasField('serial_number'):
+            #    resource.serial_number = request.configuration.serial_number
+        
+            ### This method will clear the field in the resource if it is not set in the message.
+            if request.configuration.HasField('name'):
+                resource.name = request.configuration.name
+            else:
+                resource.ClearField('name')
+                
+            if request.configuration.HasField('make'):
+                resource.make = request.configuration.make
+            else:
+                resource.ClearField('make')
+                
+            if request.configuration.HasField('model'):
+                resource.model = request.configuration.model
+            else:
+                resource.ClearField('model')
+                
+            if request.configuration.HasField('serial_number'):
+                resource.serial_number = request.configuration.serial_number
+            else:
+                resource.ClearField('serial_number')
+        
+        yield self.rc.put_instance(resource)
+        
+        log.info(str(resource))
+        
+        
+        response = yield self.mc.create_instance(resource_response_type, name='update_instrument_resource response')
+        
+        # Create a reference to return to the caller
+        # This is one pattern - it exposes the resource to the caller
+        
+        # pass the reference
+        response.resource_reference = self.rc.reference_instance(resource)
+        
+        # pass the current configuration
+        response.configuration = resource.ResourceObject
+        
+        # pass the result of the create operation...
+        response.result = 'Created'
+                
+        # The following line shows how to reply to a message
+        yield self.reply_ok(msg, response)
+        
+
+    @defer.inlineCallbacks
+    def op_set_instrument_resource_life_cycle(self, request, headers, msg):
+        """
+        This method is an example using a standard resource update request.
+        It contains two parts, the reference to the resource to update and the
+        state of the resource which should be set.
+        """
+        
+        log.info('op_update_instrument_resource: ')
+            
+        # Check only the type recieved and linked object types. All fields are
+        #strongly typed in google protocol buffers!
+        if request.MessageType != resource_request_type:
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloResourceError('Expected message type ResourceConfigurationRequest, received %s'
+                                     % str(request))
+        
+        ### Check the type of the configuration request
+        if not request.HasField('configuration'):
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloResourceError('Expected message with configuration field of type InstrumentInfoRequest, received empty configuration!')
+
+        if request.configuration.ObjectType != instrument_info_type:
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloResourceError('Expected message with configuration field of type InstrumentInfoRequest, received %s; type %s'
+                    % (str(request.configuration), str(request.configuration.ObjectClass)))
+
+        ### Check the type of the configuration request
+        if not request.HasField('resource_reference'):
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloResourceError('Expected message with resource_reference field of type IDRef, received empty configuration!')
+            
+        if request.resource_reference.ObjectType != resource_reference_type:
+
+            # This will terminate the hello service. As an alternative reply okay with an error message
+            raise HelloResourceError('Expected message with resource_reference field of type IDRef, received %s; type %s'
+                    % (str(request.resource_reference), str(request.resource_reference.ObjectClass)))
+
+
+            
+        # Get the current state of the resource
+        resource = yield self.rc.get_instance(request.resource_reference)
+        
+
+
+    
+
+
+class HelloResourceClient(ServiceClient):
+    """
+    This is an exemplar service client that calls the hello object service. It
+    makes service calls RPC style using GPB object. There is no special handling
+    here, just call send. The clients should become exteremly thin wrappers with
+    no business logic.
+    """
+
+    def __init__(self, proc=None, **kwargs):
+        if not 'targetname' in kwargs:
+            kwargs['targetname'] = "hello_resource"
+        ServiceClient.__init__(self, proc, **kwargs)
+
+    @defer.inlineCallbacks
+    def create_instrument_resource(self, msg):
+        yield self._check_init()
+        
+        (content, headers, msg) = yield self.rpc_send('create_instrument_resource', msg)
+        
+        defer.returnValue(content)
+        
+    @defer.inlineCallbacks
+    def update_instrument_resource(self, msg):
+        yield self._check_init()
+        
+        (content, headers, msg) = yield self.rpc_send('update_instrument_resource', msg)
+        
+        defer.returnValue(content)
+
+
+# Spawn of the process using the module name
+factory = ProcessFactory(HelloResource)
+
+
---
+++ ion/play/test/test_hello_resource.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+"""
+@file ion/play/test/test_hello_resource.py
+@test ion.play.hello_resource Example unit tests for sample resource code.
+@author David Stuebe
+"""
+import ion.util.ionlog
+log = ion.util.ionlog.getLogger(__name__)
+
+from twisted.internet import defer
+
+# Not required but used for inspection... The resource client is in the service!
+from ion.services.coi.resource_registry_beta.resource_client import ResourceClient
+
+from ion.play.hello_resource import HelloResourceClient
+from ion.test.iontest import IonTestCase
+
+from ion.core.messaging.message_client import MessageClient
+
+
+from ion.core.process.process import Process, ProcessClient, ProcessDesc
+from ion.core import bootstrap
+
+from ion.core.object import object_utils
+
+instrument_resource_type = object_utils.create_type_identifier(object_id=20029, version=1)
+instrument_info_type = object_utils.create_type_identifier(object_id=20030, version=1)
+resource_request_type = object_utils.create_type_identifier(object_id=10, version=1)
+resource_response_type = object_utils.create_type_identifier(object_id=12, version=1)
+
+
+class HelloResourceTest(IonTestCase):
+    """
+    Testing example hello object service.
+    This example shows how it is possible to create and send strongly typed objects
+    Each time an object is sent it is assigned a new identifier. The example
+    shows how it is possible to move a linked composite from one object to another.
+    """
+
+class HelloResourceTest(IonTestCase):
+    """
+    Testing example hello resource service.
+    This example shows how it is possible to create and send resource requests.
+    """
+
+    @defer.inlineCallbacks
+    def setUp(self):
+        yield self._start_container()
+        
+        services = [
+            {'name':'ds1','module':'ion.services.coi.datastore','class':'DataStoreService',
+             'spawnargs':{'servicename':'datastore'}},
+            {'name':'resource_registry1','module':'ion.services.coi.resource_registry_beta.resource_registry','class':'ResourceRegistryService',
+             'spawnargs':{'datastore_service':'datastore'}},
+            {'name':'hello1','module':'ion.play.hello_resource','class':'HelloResource'},
+        ]
+
+        sup = yield self._spawn_processes(services)
+        
+        self.mc = MessageClient(proc = self.test_sup)
+        
+
+    @defer.inlineCallbacks
+    def tearDown(self):
+        yield self._stop_container()
+
+
+    @defer.inlineCallbacks
+    def test_hello_resource(self):
+            
+        # Create a hello object client
+        hc1 = HelloResourceClient(proc=self.test_sup)
+        
+        create_request_msg = yield self.mc.create_instance(resource_request_type, name='Create me!')
+        
+        create_request_msg.configuration = create_request_msg.CreateObject(instrument_info_type)
+        
+        create_request_msg.configuration.name = 'A CTD'
+        create_request_msg.configuration.make = 'SeaBird'
+        create_request_msg.configuration.model = 'SBE911'
+        create_request_msg.configuration.serial_number = 'ax1d92p'
+        
+        
+        log.info('Instrument config message! \n' + str(create_request_msg))
+        
+        
+        log.info('Calling create instrument resource ')
+        # You can send the root of the object or any linked composite part of it.
+        create_response_msg = yield hc1.create_instrument_resource(create_request_msg)
+        
+        log.info('Create returned resource reference:\n%s' % str(create_response_msg))
+        
+        self.assertEqual(create_response_msg.configuration.name, 'A CTD')
+        
+        ### request to clobber the state of the resource
+        update_request_msg = yield self.mc.create_instance(resource_request_type, name='Update it!')
+        
+        # Copy from the create request and make some changes...
+        update_request_msg.configuration = create_request_msg.configuration
+        update_request_msg.configuration.name = 'Junk'
+        update_request_msg.configuration.model = 'Jabberwocky'
+        
+        # Set the reference to update
+        update_request_msg.resource_reference = create_response_msg.resource_reference
+        
+        # Call the update method 
+        update_result_msg = yield hc1.update_instrument_resource(update_request_msg)
+        
+        
+        
+        log.info('Tada!')
+        
+        
+        
+        
