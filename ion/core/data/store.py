da7b603b44e7f606ae2eeb24f91fa3351652f172
David Stuebe <dstuebe@asascience.com>
2011-02-02 17:08:07 -0500

    Added IIndexStore and in memory index store implementation. Modified return value of index query.

--- ion/core/data/store.py
+++ ion/core/data/store.py
@@ -80,6 +80,134 @@ class Store(object):
             del self.kvs[key]
         return defer.succeed(None)
 
+class IIndexStore(IStore):
+    """
+    Interface all store backend implementations.
+    All operations are returning deferreds and operate asynchronously.
+
+    @var namespace
+    """
+
+    def get(key):
+        """
+        @param key  an immutable key associated with a value
+        @retval Deferred, for value associated with key, or None if not existing.
+        """
+
+    def put(key, value, index_attributes={}):
+        """
+        @param key  an immutable key to be associated with a value
+        @param value  an object to be associated with the key. The caller must
+                not modify this object after it was
+        @retval Deferred, for success of this operation
+        """
+
+    def remove(key):
+        """
+        @param key  an immutable key associated with a value
+        @retval Deferred, for success of this operation
+     
+        """
+        
+    def query(self, indexed_attributes={}):
+        """
+        Search for rows in the Cassandra instance.
+    
+        @param indexed_attributes is a dictionary with column:value mappings.
+        Rows are returned that have columns set to the value specified in 
+        the dictionary
+        
+        @retVal a thrift representation of the rows returned by the query.
+        """
+        
+    def get_query_attributes(self):
+        """
+        Return the column names that are indexed.
+        """
+
+class IndexStore(object):
+    """
+    Memory implementation of an asynchronous key/value store, using a dict.
+    Simulates typical usage of using a client connection to a backend
+    technology.
+    """
+    implements(IIndexStore)
+
+    def __init__(self, *args, **kwargs):
+        self.kvs = {}
+        self.indices = {}
+        
+        print 'KWARGS', kwargs
+        if kwargs.has_key('indices'):
+            for name in kwargs.get('indices'):
+                self.indices[name]={}
+
+    def get(self, key):
+        """
+        @see IStore.get
+        """
+        return defer.maybeDeferred(self.kvs.get, key, None)
+
+    def put(self, key, value, index_attributes={}):
+        """
+        @see IStore.put
+        """
+        for k,v in index_attributes.items():
+            kindex = self.indices.get(k, None)
+            if not kindex:
+                kindex = {}
+                self.indices[k] = kindex
+            kindex[v]=key
+            
+        return defer.maybeDeferred(self.kvs.update, {key:value})
+
+    def remove(self, key):
+        """
+        @see IStore.remove
+        """
+        # could test for existance of key. this will error otherwise
+        if self.kvs.has_key(key):
+            del self.kvs[key]            
+        return defer.succeed(None)
+        
+    def query(self, indexed_attributes={}):
+        """
+        Search for rows in the Cassandra instance.
+    
+        @param indexed_attributes is a dictionary with column:value mappings.
+        Rows are returned that have columns set to the value specified in 
+        the dictionary
+        
+        @retVal a thrift representation of the rows returned by the query.
+        """
+        
+        return defer.maybeDeferred(self._query, indexed_attributes)
+        
+    def _query(self, indexed_attributes={}):
+        keys = set()
+        
+        for k,v in indexed_attributes.items():
+            kindex = self.indices.get(k, None)
+            if kindex:
+                keys.add(kindex.get(v,None))
+            
+        keys.discard(None)
+        
+        result = {}
+        for k in keys:
+            # This is stupid, but now remove effectively works - delete keys are no longer visible!
+            if self.kvs.has_key(k):
+                result[k] = self.kvs.get(k)
+                
+        return result
+        
+    def get_query_attributes(self):
+        """
+        Return the column names that are indexed.
+        """
+        return defer.maybeDeferred(self.indices.keys)
+        
+
 class IDataManager(Interface):
     """
     @note Proposed class to fulfill preservation service management?
