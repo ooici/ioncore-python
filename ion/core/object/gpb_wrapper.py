7475970e3d848eb88e82373dfa6307101792dfda
David Stuebe <dstuebe@asascience.com>
2011-02-05 18:29:32 -0500

    Changed repository and workbench to use only type id objects

--- ion/core/object/gpb_wrapper.py
+++ ion/core/object/gpb_wrapper.py
@@ -21,12 +21,20 @@ from google.protobuf import message
 from google.protobuf.internal import containers
 from google.protobuf import descriptor
     
-from net.ooici.core.container import container_pb2
 from net.ooici.core.link import link_pb2
 from net.ooici.core.type import type_pb2
 from ion.util.cache import memoize
 
 import hashlib
+
+structure_element_type = object_utils.create_type_identifier(object_id=1, version=1)
+structure_type = object_utils.create_type_identifier(object_id=2, version=1)
+
+idref_type = object_utils.create_type_identifier(object_id=4, version=1)
+gpbtype_type = object_utils.create_type_identifier(object_id=9, version=1)
+
+
+
 
 class OOIObjectError(Exception):
     """
@@ -1422,7 +1430,7 @@ class StructureElement(object):
     """
     def __init__(self):
             
-        self._element = container_pb2.StructureElement()
+        self._element = object_utils.get_gpb_class_from_type_id(structure_element_type)()
         self.ChildLinks = set()
         
     @classmethod
