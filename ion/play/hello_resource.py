7b860a1eca53ea95abae36c771451492f3f474e5
David Stuebe <dstuebe@asascience.com>
2011-02-08 11:08:23 -0500

    Updated proto dependency and fixed resource app to use new style enums.

--- ion/play/hello_resource.py
+++ ion/play/hello_resource.py
@@ -383,19 +383,19 @@ class HelloResource(ServiceProcess):
         if request.life_cycle_operation == request.MessageObject.LifeCycleOperation.ACTIVATE:
            resource.ResourceLifeCycleState = resource.ACTIVE
 
-        elif request.life_cycle_operation == request.MessageObject.Deactivate:
+        elif request.life_cycle_operation == request.MessageObject.LifeCycleOperation.DEACTIVATE:
            resource.ResourceLifeCycleState = resource.INACTIVE
            
-        elif request.life_cycle_operation == request.MessageObject.Commission:
+        elif request.life_cycle_operation == request.MessageObject.LifeCycleOperation.COMMISSION:
            resource.ResourceLifeCycleState = resource.COMMISSIONED
            
-        elif request.life_cycle_operation == request.MessageObject.Decommission:
+        elif request.life_cycle_operation == request.MessageObject.LifeCycleOperation.Decommission:
            resource.ResourceLifeCycleState = resource.DECOMMISSIONED
            
-        elif request.life_cycle_operation == request.MessageObject.Retire:
+        elif request.life_cycle_operation == request.MessageObject.LifeCycleOperation.Retire:
            resource.ResourceLifeCycleState = resource.RETIRED
            
-        elif request.life_cycle_operation == request.MessageObject.Develop:
+        elif request.life_cycle_operation == request.MessageObject.LifeCycleOperation.Develop:
            resource.ResourceLifeCycleState = resource.DEVELOPED
 
         yield self.rc.put_instance(resource)
