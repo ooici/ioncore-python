6048ce0a8df86ea93a8e3ccb8eae2e921b2b048b
David Stuebe <dstuebe@asascience.com>
2011-02-21 21:25:59 -0800

    Fixed last places where name was changed to ResourceName/MessageName

--- ion/services/dm/ingestion/eoi_ingester.py
+++ ion/services/dm/ingestion/eoi_ingester.py
@@ -74,9 +74,9 @@ class EOIIngestionService(ServiceProcess):
         
         msg_repo = content.Repository
         
-        response, exception = yield self.push('datastore', msg_repo.repository_key)
+        result = yield self.push('datastore', msg_repo.repository_key)
         
-        assert response == self.ION_SUCCESS, 'Push to datastore failed!'
+        assert result.MessageResponseCode == result.ResponseCodes.OK, 'Push to datastore failed!'
         
         yield self.reply(msg, content=msg_repo.repository_key)
         
@@ -89,9 +89,9 @@ class EOIIngestionService(ServiceProcess):
         Content is the unique ID for a particular dataset
         """
         log.debug('op_retrieve: recieved content:'+ str(content))
-        response, exception = yield self.pull('datastore', str(content))
+        result = yield self.pull('datastore', str(content))
         
-        assert response == self.ION_SUCCESS, 'Push to datastore failed!'
+        assert result.MessageResponseCode == result.ResponseCodes.OK, 'Push to datastore failed!'
         
         repo = self.workbench.get_repository(content)
         
@@ -132,12 +132,8 @@ class EOIIngestionClient(ServiceClient):
         
         (content, headers, msg) = yield self.rpc_send('ingest', ab)
         
-        response = headers.get(self.MSG_RESPONSE)
-        exception = headers.get(self.MSG_EXCEPTION)
-        
-        log.info('EOI Ingestion Service; Ingest replied: %s, ID: %s' % (response, str(content)))
 
-        defer.returnValue((response, content))
+        defer.returnValue(content)
         
         
 
@@ -155,7 +151,7 @@ class EOIIngestionClient(ServiceClient):
         
         log.info('EOI Ingestion Service; Retrieve replied: '+str(content))
         # Return value should be a resource identity
-        defer.returnValue(str(content))
+        defer.returnValue(content)
         
 
 # Spawn of the process using the module name
