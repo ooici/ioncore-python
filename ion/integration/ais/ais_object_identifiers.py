3e9d9fa144dd441ebbde6054c41c710426793073
Bill Bollenbacher <wbollenbacher@ucsd.edu>
2011-03-11 11:28:29 -0800

    changed RegisterIonUser name

--- ion/integration/ais/ais_object_identifiers.py
+++ ion/integration/ais/ais_object_identifiers.py
@@ -63,7 +63,7 @@ message FindDataResourcesMsg {
 # RegisterUser GPBs from ion-object-definitions/net/ooici/integration/ais/registerUser/register_user.proto
 REGISTER_USER_TYPE = object_utils.create_type_identifier(object_id=9101, version=1)
 """
-message RegisterUser {
+message RegisterIonUser {
    enum _MessageTypeIdentifier {
        _ID = 9101;
        _VERSION = 1;
