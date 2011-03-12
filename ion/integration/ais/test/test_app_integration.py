c8dafe0c18e71acfd4812ef9c9c115408242f5ae
Bill Bollenbacher <wbollenbacher@ucsd.edu>
2011-03-11 16:15:42 -0800

    fixed bug introduced in last check in

--- ion/integration/ais/test/test_app_integration.py
+++ ion/integration/ais/test/test_app_integration.py
@@ -38,7 +38,7 @@ class AppIntegrationTest(IonTestCase):
     def setUp(self):
         yield self._start_container()
         services = [
-            #{'name':'app_integration','module':'ion.integration.ais.app_integration_service','class':'AppIntegrationService'},
+            {'name':'app_integration','module':'ion.integration.ais.app_integration_service','class':'AppIntegrationService'},
             {'name':'ds1','module':'ion.services.coi.datastore','class':'DataStoreService',
              'spawnargs':{'servicename':'datastore'}},
             {'name':'resource_registry1','module':'ion.services.coi.resource_registry_beta.resource_registry','class':'ResourceRegistryService',
