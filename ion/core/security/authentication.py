67c3e38f7e3a787b2d7f5fc5290f9e40cfe47b44
Bill Bollenbacher <wbollenbacher@ucsd.edu>
2011-03-14 13:10:03 -0700

    save

--- ion/core/security/authentication.py
+++ ion/core/security/authentication.py
@@ -145,6 +145,7 @@ class Authentication(object):
         Return a Dict of all known attributes for the certificate
         """
         log.info("decode_certificate:\n"+str(certificate))
+        print certificate
         attributes = {}
         x509 = X509.load_cert_string(certificate, format=1)
         
