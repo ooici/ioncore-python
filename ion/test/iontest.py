2b90c340062b4905603cb3627d6f1d76e22bde8c
Dave Foster <dfoster@asascience.com>
2011-03-23 10:53:42 -0400

    Env var override for broker address in test cases

--- ion/test/iontest.py
+++ ion/test/iontest.py
@@ -55,7 +55,12 @@ class IonTestCase(unittest.TestCase):
         """
         #mopt = {}
         mopt = service.Options()
-        mopt['broker_host'] = CONF['broker_host']
+        if os.environ.has_key("ION_TEST_CASE_BROKER_HOST"):
+            host = os.environ["ION_TEST_CASE_BROKER_HOST"]
+            log.debug("Using environment set ION_TEST_CASE_BROKER_HOST (%s)" % host)
+            mopt["broker_host"] = host
+        else:
+            mopt['broker_host'] = CONF['broker_host']
         mopt['broker_port'] = CONF['broker_port']
         mopt['broker_vhost'] = CONF['broker_vhost']
         mopt['broker_heartbeat'] = CONF['broker_heartbeat']
