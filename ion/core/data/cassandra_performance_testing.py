164e942fdc54c8133a99ad8b09e6b24e3f48fc08
Matt Rodriguez <mateo@Matt-Rodriguezs-MacBook-Pro.local>
2011-03-24 12:57:14 -0700

    Added new query tests to cassandra_performance_testing. Fixed how index_attributes are passed in the put method, added default row_count argument in query which limits how many rows can be returned.

--- ion/core/data/cassandra_performance_testing.py
+++ ion/core/data/cassandra_performance_testing.py
@@ -2,6 +2,9 @@
 @file ion/core/data/cassandra_performance_testing.py
 @author Matt Rodriguez
 @brief Generates cassandra-cli commands given the storage.cfg file as input.
+
+This standalone script can probably be broken into a few different scripts. Currently 
+the design can kindly be described as results-oriented.
 """
 from ion.core.data.cassandra_bootstrap import CassandraStoreBootstrap, CassandraIndexedStoreBootstrap
 from ion.core.data.store import Query
@@ -23,6 +26,7 @@ class CassandraPerformanceTester:
     
     def __init__(self, index=False, num_rows = 100, blob_size=MB):
         self.index = index
+        self.blob_size = blob_size
         if self.index:
             self.store = CassandraIndexedStoreBootstrap("ooiuser", "oceans11")
         else:
@@ -34,7 +38,7 @@ class CassandraPerformanceTester:
         f = open("/dev/urandom")  
         t1 = time.time()
         for i in range(self.num_rows):
-            blob = f.read(blob_size)
+            blob = f.read(self.blob_size)
             key = sha.sha(blob).digest()
             self.blobs.update({key:blob})
         t2 = time.time()
@@ -44,7 +48,10 @@ class CassandraPerformanceTester:
         #Have the store connect to the Cassandra cluster
         self.store.initialize()
         self.store.activate()
-        self.indexes = []
+        self.indexes = ["branch_name","keyword","object_branch",
+                   "object_commit","object_key","predicate_branch",
+                   "predicate_commit","predicate_key","repository_key",
+                   "subject_branch","subject_commit","subject_key" ]
         self.index_values_dict = {}
         
         
@@ -61,6 +68,98 @@ class CassandraPerformanceTester:
         yield  self.tearDown()    
     
     @defer.inlineCallbacks
+    def runBenchmarkTests(self):
+        yield self.setUp()
+        
+        yield self.get_benchmark(50)
+        yield self.has_key_benchmark(50)
+        yield self.put_benchmark(50)
+        if self.index:
+            yield self.update_index_benchmark(50)
+        yield self.tearDown()    
+        
+
+    @defer.inlineCallbacks
+    def runQueryBenchMarks(self):
+        
+        
+        @defer.inlineCallbacks
+        def setup_indexes(f, rows_returned):
+            b1 = f.read(1024)
+            b2 = f.read(1024)
+            search_term1 =  sha.sha(b1).digest()
+            search_term2 = sha.sha(b2).digest()
+            index_values = [search_term1, search_term2, "1"]
+            b_random = f.read(1024)
+            random_values = [ sha.sha(b_random).digest() for i in range(9) ]
+            index_values.extend(random_values)
+            index_attributes = dict(zip(self.indexes, index_values))
+            
+            for i in range(rows_returned):
+                k,v = self.query_blobs.popitem()
+                yield self.store.put(k,v,index_attributes)
+            
+            defer.returnValue((search_term1, search_term2))
+        
+        @defer.inlineCallbacks
+        def put_rows(f):
+            b1 = f.read(1024)
+            index_val = sha.sha(b1).digest()
+            random_values = [ index_val for i in range(len(self.indexes))]
+            index_attributes = dict(zip(self.indexes, random_values))
+            for k,v in self.query_blobs.items():
+                yield self.store.put(k,v,index_attributes)
+ 
+        @defer.inlineCallbacks
+        def runQuery(q, pred_type):
+            for i in range(50):
+                t1 = time.time()
+                rows = yield self.store.query(q)
+                t2 = time.time()
+                diff = t2 - t1
+                print "Returns %s_rows in %s query %s " % (len(rows), diff, pred_type)
+                #print "%s rows returned." % (len(rows))
+        
+        @defer.inlineCallbacks
+        def runQueries(terms):
+            q1 = Query()
+            q1.add_predicate_eq(self.indexes[0], terms[0])
+            yield runQuery(q1, "1_EQ")
+            
+            q1.add_predicate_eq(self.indexes[1], terms[1])
+            yield runQuery(q1, "2_EQ")
+            q1.add_predicate_gt(self.indexes[2], "")
+            yield runQuery(q1, "2_EQ_1_GT")
+            q2 = Query()
+            q2.add_predicate_eq(self.indexes[0], terms[0])
+            q2.add_predicate_gt(self.indexes[2], "")
+            yield runQuery(q2, "1_EQ_1_GT")
+            
+        
+        self.query_blobs = dict(**self.blobs)
+        f = open("/dev/urandom")
+        t1 = time.time()
+        terms1 = yield setup_indexes(f,1)
+        terms10 = yield setup_indexes(f,10)
+        terms100 = yield setup_indexes(f,100)
+        terms1000 = yield setup_indexes(f,1000)
+    
+        t2 = time.time()
+        diff = t2 - t1
+        print "Time to setup indexes: %s " % (diff,)
+        t1 = time.time()
+        yield put_rows(f)
+        t2 = time.time()
+        diff = t2 - t1
+        print "Time to put rows: %s " % (diff,)
+        yield runQueries(terms1)
+        yield runQueries(terms10)
+        yield runQueries(terms100)
+        yield runQueries(terms1000)
+        yield self.tearDown()
+        
+    
+    @defer.inlineCallbacks
     def setUp(self):
         """
         This puts the blobs into the Cassandra cluster
@@ -72,7 +171,27 @@ class CassandraPerformanceTester:
         diff =  t2 - t1
         print "Time to do %s puts %s " % (len(self.blobs),diff)
         
-    
+    @defer.inlineCallbacks
+    def put_benchmark(self, ops):
+        
+        
+        d = {}
+        f = open("/dev/urandom")  
+        for i in range(ops):
+            blob = f.read(self.blob_size)
+            key = sha.sha(blob).digest()
+            d.update({key:blob})
+        
+        for k,v in d.items():
+            t1 = time.time()
+            yield self.store.put(k,v)
+            t2 = time.time()
+            diff = t2 - t1
+            print "Time to do put %s " % (diff,)  
+            
+        for k in d.keys():
+            yield self.store.remove(k)      
+            
     @defer.inlineCallbacks
     def query(self):
         """
@@ -138,16 +257,13 @@ class CassandraPerformanceTester:
         
         
         
-    @defer.inlineCallbacks
-    def update_index(self):
+   
+    def setup_update_index(self):
         """
         The first index has a cardinality of 1, the second has a cardinality of 2, and 
         the 12th has a cardinality of 12. 
         """
-        self.indexes = ["branch_name","keyword","object_branch",
-                   "object_commit","object_key","predicate_branch",
-                   "predicate_commit","predicate_key","repository_key",
-                   "subject_branch","subject_commit","subject_key" ]
+        
         
         indexes_mod_dict = dict(zip(self.indexes, range(1, len(self.indexes) + 1)))
         
@@ -167,7 +283,22 @@ class CassandraPerformanceTester:
             update_requests.append(attributes)
         
         update_requests_dict = dict(zip(self.blobs.keys(), update_requests))
-        
+        return update_requests_dict
+    
+    @defer.inlineCallbacks
+    def update_index_benchmark(self, ops):
+        update_requests_dict = self.setup_update_index()
+        for i in range(ops):
+            k,v = update_requests_dict.popitem()
+            t1 = time.time()
+            yield self.store.update_index(k,v)
+            t2 = time.time()
+            diff = t2 - t1
+            print "Update index time: %s " % (diff,)
+    
+    @defer.inlineCallbacks
+    def update_index(self):
+        update_requests_dict = self.setup_update_index()
         t1 = time.time()
         for k,v in update_requests_dict.items():
             yield self.store.update_index(k, v)
@@ -175,6 +306,16 @@ class CassandraPerformanceTester:
         diff = t2 - t1
         print "Time to do %s update_index calls %s " % (len(self.blobs), diff)
                 
+    @defer.inlineCallbacks
+    def has_key_benchmark(self, ops):
+        test_keys = self.get_key_number(ops)
+        for key in test_keys:
+            t1 = time.time()
+            yield self.store.has_key(key)  
+            t2 = time.time()
+            diff = t2 - t1
+            print "Has key time: %s " % (diff,) 
+                     
     @defer.inlineCallbacks
     def has_key(self):
         """
@@ -207,6 +348,15 @@ class CassandraPerformanceTester:
         diff = t2 - t1
         print "Time to do %s has_keys with negatives: %s " % (len(no_keys), diff)   
            
+    def get_key_number(self, key_number):       
+        keys = self.blobs.keys()
+        random.shuffle(keys)
+        test_keys = []
+        for i in range(key_number):
+            test_keys.append(keys.pop())
+        
+        return test_keys
+        
     def get_key_fraction(self, keys_fraction):
         
         keys = self.blobs.keys()
@@ -217,6 +367,17 @@ class CassandraPerformanceTester:
             test_keys.append(keys.pop())
             
         return test_keys
+    
+    @defer.inlineCallbacks
+    def get_benchmark(self, ops):
+        test_keys = self.get_key_number(ops)
+        
+        for k in test_keys:
+            t1 = time.time()
+            val = yield self.store.get(k)
+            t2 = time.time()
+            diff = t2 - t1
+            print "Time to do a get: %s " % ( diff,)
         
     @defer.inlineCallbacks
     def gets(self):
@@ -246,10 +407,11 @@ def main():
     parser = OptionParser()
     parser.add_option("-b", "--blobs", dest="blobs", default=100, help="The number of blobs or rows to put into Cassandra")
     parser.add_option("-s", "--size", dest="size", default=MB, help="The number of blobs or rows to put into Cassandra")
+    parser.add_option("-i", "--indexed", action="store_true", dest="indexed", default=False, help="Use the indexed column family or the nonindexed column family")
     opts, args = parser.parse_args()
-
-    tester = CassandraPerformanceTester(index=True,num_rows=int(opts.blobs),blob_size=int(opts.size))
-    tester.runTests()
+    tester = CassandraPerformanceTester(index=opts.indexed, num_rows=int(opts.blobs),blob_size=int(opts.size))
+    #tester.runBenchmarkTests()
+    tester.runQueryBenchMarks()
     reactor.run() 
     
 if __name__ == "__main__":
