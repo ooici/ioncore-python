8902b37d82d002da77c6451486ef2c5a61414ae3
Dave Foster <dfoster@asascience.com>
2011-04-04 15:32:36 -0400

    Restoring needed args/kwargs to Publisher/Subscriber methods

--- ion/services/dm/distribution/publisher_subscriber.py
+++ ion/services/dm/distribution/publisher_subscriber.py
@@ -25,7 +25,7 @@ class Publisher(BasicLifecycleObject):
     to be instantiated within another class/process/codebase, as an object for sending data to OOI.
     """
 
-    def __init__(self, xp_name=None, routing_key=None, credentials=None, process=None):
+    def __init__(self, xp_name=None, routing_key=None, credentials=None, process=None, *args, **kwargs):
 
         BasicLifecycleObject.__init__(self)
 
@@ -116,7 +116,7 @@ class PublisherFactory(object):
         self._publisher_id = None
 
     @defer.inlineCallbacks
-    def build(self, routing_key, xp_name=None, credentials=None, process=None, publisher_type=None):
+    def build(self, routing_key, xp_name=None, credentials=None, process=None, publisher_type=None, *args, **kwargs):
         """
         Creates a publisher and calls register on it.
 
@@ -139,7 +139,7 @@ class PublisherFactory(object):
         publisher_type  = publisher_type or self._publisher_type or Publisher
         publisher_name  = 'Publisher'
 
-        pub = publisher_type(xp_name=xp_name, routing_key=routing_key, credentials=credentials, process=process)
+        pub = publisher_type(xp_name=xp_name, routing_key=routing_key, credentials=credentials, process=process, *args, **kwargs)
         yield process.register_life_cycle_object(pub)     # brings the publisher to whatever state the process is in
 
         # Register does the PSC invocations
@@ -374,8 +374,7 @@ class SubscriberFactory(object):
         self._credentials       = credentials
 
     @defer.inlineCallbacks
-    def build(self, xp_name=None, binding_key=None, queue_name=None, handler=None, subscriber_type=Subscriber,
-              process=None, credentials=None):
+    def build(self, xp_name=None, binding_key=None, queue_name=None, handler=None, subscriber_type=Subscriber, process=None, credentials=None, *args, **kwargs):
         """
         Creates a subscriber.
 
@@ -409,7 +408,7 @@ class SubscriberFactory(object):
         credentials     = credentials or self._credentials
 
         sub = subscriber_type(xp_name=xp_name, binding_key=binding_key, queue_name=queue_name,
-                              process=process, credentials=credentials)
+                              process=process, credentials=credentials, *args, **kwargs)
         yield sub.register()
         # brings the subscriber up to the same state as the process
         yield process.register_life_cycle_object(sub)
