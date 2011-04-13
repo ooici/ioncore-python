bc87ccc11d8326a4c6e96e92d5d8493e174e6604
David Stuebe <dstuebe@asascience.com>
2011-04-13 17:45:06 -0400

    Added skeleton for merge helper methods in ingestion

---
+++ ion/services/dm/ingestion/cdm_variable_methods.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""
+@file ion/services/dm/ingestion/cdm_variable_methods.py
+@author David Stuebe
+
+@Brief Methods for merging and access to CDM Dataset variables
+
+Based on Porter - Duff Alpha Composite rules
+"""
+
+
+
+
+
+
+
+
