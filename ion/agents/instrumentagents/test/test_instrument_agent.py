f67f9185a72374b908332948b20f9ca87b9b50a6
David Stuebe <dstuebe@asascience.com>
2011-04-29 19:32:50 -0400

    Fixed test to work with new cache policy for objects
    Signed-off-by: David Stuebe <dstuebe@asascience.com>

--- ion/agents/instrumentagents/test/test_instrument_agent.py
+++ ion/agents/instrumentagents/test/test_instrument_agent.py
@@ -1201,6 +1201,10 @@ class TestInstrumentAgent(IonTestCase):
             def ondata(self, data):
                 log.debug("TestEventSubscriber received a message with name: %s",
                           data['content'].name)
+                content = data['content']
+                if hasattr(content, 'Repository'):
+                    content.Repository.persistent = True
+                
                 self.msgs.append(data)
                 
         subproc = Process()
