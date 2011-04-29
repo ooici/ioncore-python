c8c9b6d8a5caf3e4afda8da02fd49c48ed211955
Maurice Manning <mmmanning@ucsd.edu>
2011-04-28 23:17:10 -0700

    Cleanup

--- ion/integration/ais/test/test_notification_receiver.py
+++ ion/integration/ais/test/test_notification_receiver.py
@@ -102,7 +102,7 @@ class NotificationReceiverTest(IonTestCase):
         yield self._stop_container()
 
     @defer.inlineCallbacks
-    def Xtest_publish_recieve(self):
+    def test_publish_recieve(self):
 
         log.info('NotificationReceiverTest:test_publish_receive begin')
 
@@ -111,23 +111,26 @@ class NotificationReceiverTest(IonTestCase):
 
         yield self.createUser()
 
-        # create the register_user request GPBs
-        reqMsg = yield mc.create_instance(AIS_REQUEST_MSG_TYPE, MessageName='NAS Add Subscription request')
+        # Add a subscription for this user to this data resource
+        reqMsg = yield mc.create_instance(AIS_REQUEST_MSG_TYPE)
         reqMsg.message_parameters_reference = reqMsg.CreateObject(SUBSCRIBE_DATA_RESOURCE_REQ_TYPE)
         reqMsg.message_parameters_reference.subscriptionInfo.user_ooi_id = self.user_id
         reqMsg.message_parameters_reference.subscriptionInfo.data_src_id = 'dataresrc123'
-        reqMsg.message_parameters_reference.subscriptionInfo.subscription_type = reqMsg.message_parameters_reference.subscriptionInfo.SubscriptionType.EMAIL
+        reqMsg.message_parameters_reference.subscriptionInfo.subscription_type = reqMsg.message_parameters_reference.subscriptionInfo.SubscriptionType.EMAILANDDISPATCHER
         reqMsg.message_parameters_reference.subscriptionInfo.email_alerts_filter = reqMsg.message_parameters_reference.subscriptionInfo.AlertsFilter.UPDATESANDDATASOURCEOFFLINE
 
-        log.info("NotificationReceiverTest: call the service")
+        reqMsg.message_parameters_reference.datasetMetadata.ion_time_coverage_start = '2007-01-1T00:02:00Z'
+        reqMsg.message_parameters_reference.datasetMetadata.ion_time_coverage_end = '2007-01-1T00:03:00Z'
+        reqMsg.message_parameters_reference.datasetMetadata.ion_geospatial_lat_min = -50.0
+        reqMsg.message_parameters_reference.datasetMetadata.ion_geospatial_lat_max = -40.0
+        reqMsg.message_parameters_reference.datasetMetadata.ion_geospatial_lon_min = 20.0
+        reqMsg.message_parameters_reference.datasetMetadata.ion_geospatial_lon_max = 30.0
+
         # try to register this user for the first time
         reply = yield self.nac.addSubscription(reqMsg)
-        log.info('addSubscription returned:\n'+str(reply))
 
         if reply.MessageType != AIS_RESPONSE_MSG_TYPE:
             self.fail('NotificationReceiverTest: response is not an AIS_RESPONSE_MSG_TYPE GPB')
-
-        log.info('NotificationReceiverTest: test_publish_recieve publish notification')
 
         pubSupplementAdded = DatasetSupplementAddedEventPublisher(process=self.test_sup) # all publishers/subscribers need a process associated
         yield pubSupplementAdded.initialize()
@@ -139,6 +142,7 @@ class NotificationReceiverTest(IonTestCase):
 
         # creates the event notification for us and sends it
 
+
         yield pubSupplementAdded.create_and_publish_event(origin="magnet_topic",
                                            dataset_id="dataresrc123",
                                            datasource_id="dataresrc123",
@@ -149,11 +153,7 @@ class NotificationReceiverTest(IonTestCase):
                                            datasource_id="dataresrc123",
                                            explanation="explanation")
 
-        log.info('NotificationReceiverTest: test_publish_recieve sleep')
         yield pu.asleep(3.0)
-
-
-        log.info('NotificationReceiverTest: test_publish_recieve completed')
 
 
 
