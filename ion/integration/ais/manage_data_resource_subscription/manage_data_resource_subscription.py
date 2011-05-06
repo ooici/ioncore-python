b873d481c9ee58579657c006092879cbf5787f6a
David Everett <deverett@interon.com>
2011-05-06 15:59:26 -0700

    Started work on filtering result of findDataResourceSubscriptions

--- ion/integration/ais/manage_data_resource_subscription/manage_data_resource_subscription.py
+++ ion/integration/ais/manage_data_resource_subscription/manage_data_resource_subscription.py
@@ -475,7 +475,6 @@ class ManageDataResourceSubscription(object):
             numSubsReturned = len(reply.message_parameters_reference[0].subscriptionListResults)
             log.debug('getSubscriptionList returned: ' + str(numSubsReturned) + ' subscriptions.')
 
-            defer.returnValue(reply)
 
         except ReceivedApplicationError, ex:
             log.info('ManageDataResourceSubscription.createDataResourceSubscription(): Error attempting to addSubscription(): %s' %ex)
@@ -486,6 +485,11 @@ class ManageDataResourceSubscription(object):
             Response.error_str =  ex.msg_content.MessageResponseBody
             defer.returnValue(Response)
 
+        #
+        # Now iterate through the list, filtering by the bounds
+        #
+
+        defer.returnValue(reply)
 
     @defer.inlineCallbacks
     def __createDispatcherWorkflow(self, createInfo, dispatcherID):
