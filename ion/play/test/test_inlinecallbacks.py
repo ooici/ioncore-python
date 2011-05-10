ca5682ff209b0f8a91dfe7f13dd56897e9e9a4e8
Adam R. Smith <a2smith@ucsd.edu>
2011-05-10 11:50:01 -0700

    perf test for inline callbacks

---
+++ ion/play/test/test_inlinecallbacks.py
@@ -0,0 +1,72 @@
+from twisted.trial import unittest
+from twisted.internet import defer, reactor
+import time
+
+class Yields(unittest.TestCase):
+    runs = 10000
+
+    def setUp(self):
+        self.start_time = time.time()
+        self.incrementer = 0
+
+    def tearDown(self):
+        self.end_time = time.time()
+        print 'Took %.2f seconds' % (self.end_time - self.start_time)
+
+    def _thing(self, i):
+        self.incrementer += 1
+        #print 'i: %d' % (self.incrementer)
+
+    def _defer_thing(self):
+        d = defer.Deferred()
+        d.addCallback(self._thing)
+        reactor.callLater(0, d.callback, self.incrementer)
+        return d
+
+    @defer.inlineCallbacks
+    def _yield_thing(self):
+        yield self._defer_thing()
+
+    @defer.inlineCallbacks
+    def flat(self):
+        ds = [self._defer_thing() for i in range(self.runs)]
+        yield defer.DeferredList(ds)
+
+    @defer.inlineCallbacks
+    def wrap10call(self):
+        def multiwrap(func, depth):
+            for i in xrange(depth):
+                def wrap(func):
+                    def wrapper():
+                        return func()
+                    return wrapper
+                    
+                func = wrap(func)
+            return func
+
+        func = multiwrap(self._defer_thing, 10)
+        ds = [func() for i in range(self.runs)]
+        yield defer.DeferredList(ds)
+
+    @defer.inlineCallbacks
+    def wrap10yield(self):
+        def multiwrap(func, depth):
+            for i in xrange(depth):
+                def wrap(func):
+                    @defer.inlineCallbacks
+                    def wrapper():
+                        yield func()
+                    return wrapper
+
+                func = wrap(func)
+            return func
+
+        func = multiwrap(self._defer_thing, 10)
+        ds = [func() for i in range(self.runs)]
+        yield defer.DeferredList(ds)
+
+    @defer.inlineCallbacks
+    def wrap1yield(self):
+        ds = [self._yield_thing() for i in range(self.runs)]
+        yield defer.DeferredList(ds)
+    
