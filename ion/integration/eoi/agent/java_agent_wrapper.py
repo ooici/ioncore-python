728ef72ce68e0b8fa5f53d13a9e02ccde1d6b7a4
David Stuebe <dstuebe@asascience.com>
2011-05-15 14:44:45 -0400

    updated java dataset agent version

--- ion/integration/eoi/agent/java_agent_wrapper.py
+++ ion/integration/eoi/agent/java_agent_wrapper.py
@@ -704,7 +704,7 @@ class JavaAgentWrapper(ServiceProcess):
         '''
         # @todo: Generate jar_pathname dynamically
         # jar_pathname = "/Users/tlarocque/Development/Java/Workspace_eclipse/EOI_dev/build/TryAgent.jar"   # STAR #
-        jar_pathname = CONF.getValue('dataset_agent_jar_path', 'lib/eoi-agents-0.3.3.jar')
+        jar_pathname = CONF.getValue('dataset_agent_jar_path', 'lib/eoi-agents-0.3.4.jar')
 
         if not os.path.exists(jar_pathname):
             log.error("JAR for dataset agent (%s) not found" % jar_pathname)
