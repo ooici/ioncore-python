3cfed220b629d3d4a97114fbd9dbb692892a62cc
Adam R. Smith <a2smith@ucsd.edu>
2011-05-18 11:17:02 -0700

    make the test pass without gevent

--- ion/play/test/test_inlinecallbacks_gevent.py
+++ ion/play/test/test_inlinecallbacks_gevent.py
@@ -1,79 +1,83 @@
-from gevent import monkey; monkey.patch_all()
-import gevent
-from greenlet import greenlet
-import warnings; warnings.filterwarnings("ignore")
-from gevent import rawgreenlet
-import unittest
-import time
+try:
 
-raw = True
-spawn = gevent.spawn_raw if raw else gevent.spawn
-join = rawgreenlet.join if raw else lambda g: g.join()
-joinall = rawgreenlet.joinall if raw else gevent.joinall
+    from gevent import monkey; monkey.patch_all()
+    import gevent
+    from greenlet import greenlet
+    import warnings; warnings.filterwarnings("ignore")
+    from gevent import rawgreenlet
+    import unittest
+    import time
 
-class Greenlets(unittest.TestCase):
-    runs = 10000
+    raw = True
+    spawn = gevent.spawn_raw if raw else gevent.spawn
+    join = rawgreenlet.join if raw else lambda g: g.join()
+    joinall = rawgreenlet.joinall if raw else gevent.joinall
 
-    def setUp(self):
-        self.start_time = time.time()
-        self.incrementer = 0
+    class Greenlets(unittest.TestCase):
+        runs = 10000
 
-    def tearDown(self):
-        self.end_time = time.time()
-        print 'Took %.2f seconds' % (self.end_time - self.start_time)
+        def setUp(self):
+            self.start_time = time.time()
+            self.incrementer = 0
 
-    def _thing(self, i=0):
-        self.incrementer += 1
-        #print 'i: %d' % (self.incrementer)
+        def tearDown(self):
+            self.end_time = time.time()
+            print 'Took %.2f seconds' % (self.end_time - self.start_time)
 
-    def _spawn_thing(self):
-        g = spawn(self._thing)
-        return g
+        def _thing(self, i=0):
+            self.incrementer += 1
+            #print 'i: %d' % (self.incrementer)
 
-    def _join_thing(self):
-        g = self._spawn_thing()
-        return join(g)
+        def _spawn_thing(self):
+            g = spawn(self._thing)
+            return g
 
-    def flat_call(self):
-        [self._thing(0) for i in xrange(self.runs)]
+        def _join_thing(self):
+            g = self._spawn_thing()
+            return join(g)
 
-    def flat_spawn(self):
-        gs = [self._spawn_thing() for i in xrange(self.runs)]
-        joinall(gs)
+        def flat_call(self):
+            [self._thing(0) for i in xrange(self.runs)]
 
-    def wrap10call(self):
-        def multiwrap(func, depth):
-            for i in xrange(depth):
-                def wrap(func):
-                    def wrapper():
-                        return func()
-                    return wrapper
-                    
-                func = wrap(func)
-            return func
+        def flat_spawn(self):
+            gs = [self._spawn_thing() for i in xrange(self.runs)]
+            joinall(gs)
 
-        func = multiwrap(self._spawn_thing, 10)
-        gs = [func() for i in xrange(self.runs)]
-        joinall(gs)
+        def wrap10call(self):
+            def multiwrap(func, depth):
+                for i in xrange(depth):
+                    def wrap(func):
+                        def wrapper():
+                            return func()
+                        return wrapper
 
-    def wrap10join(self):
-        ''' There is no reason to ever do this in gevent '''
-        
-        def multiwrap(func, depth):
-            for i in xrange(depth):
-                def wrap(func):
-                    def wrapper():
-                        join(spawn(func))
-                    return wrapper
+                    func = wrap(func)
+                return func
 
-                func = wrap(func)
-            return func
+            func = multiwrap(self._spawn_thing, 10)
+            gs = [func() for i in xrange(self.runs)]
+            joinall(gs)
 
-        func = multiwrap(self._spawn_thing, 10)
-        gs = [spawn(func) for i in xrange(self.runs)]
-        joinall(gs)
+        def wrap10join(self):
+            ''' There is no reason to ever do this in gevent '''
 
-    def wrap1join(self):
-        gs = [spawn(self._join_thing) for i in xrange(self.runs)]
-        joinall(gs)
-    
+            def multiwrap(func, depth):
+                for i in xrange(depth):
+                    def wrap(func):
+                        def wrapper():
+                            join(spawn(func))
+                        return wrapper
+
+                    func = wrap(func)
+                return func
+
+            func = multiwrap(self._spawn_thing, 10)
+            gs = [spawn(func) for i in xrange(self.runs)]
+            joinall(gs)
+
+        def wrap1join(self):
+            gs = [spawn(self._join_thing) for i in xrange(self.runs)]
+            joinall(gs)
+
+except ImportError:
+    pass
