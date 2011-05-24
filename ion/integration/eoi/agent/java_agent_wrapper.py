f3ed4ffa0bf4bfa3e3e59ab79911dd4a28e884d3
David Stuebe <dstuebe@asascience.com>
2011-05-24 01:41:04 -0400

    Fixed possible race condition causing attribute error on increase_timeout

--- ion/integration/eoi/agent/java_agent_wrapper.py
+++ ion/integration/eoi/agent/java_agent_wrapper.py
@@ -404,7 +404,8 @@ class JavaAgentWrapper(ServiceProcess):
         def increase_timeout(data):
             log.debug("INCREASING TIMEOUT")
             log.debug(dir(perform_ingest_deferred))
-            perform_ingest_deferred.rpc_call.delay(ingest_timeout/1000)
+            if hasattr(perform_ingest_deferred, 'rpc_call'):
+                perform_ingest_deferred.rpc_call.delay(ingest_timeout/1000)
 
         self._subscriber.ondata = increase_timeout
 
