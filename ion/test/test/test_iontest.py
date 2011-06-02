00c4a872dfc32ab68ccfab45771f70e4ea2f3ae8
Dorian Raymer <deldotdr@gmail.com>
2011-06-02 16:52:25 -0700

    Comment new test suite

--- ion/test/test/test_iontest.py
+++ ion/test/test/test_iontest.py
@@ -1,4 +1,11 @@
+"""
+@file ion/test/test/test_iontest.py
+@author Dorian Raymer
+@brief Tests for stability between IonTestCase runs (because the state of the
+container for any given test is coupled to the that of the previous tests!)
 
+@note These tests should not be run with the random order option!!!
+"""
 from twisted.internet import defer
 
 from ion.test import iontest
@@ -34,6 +41,13 @@ class TestFailStartContainer:#(iontest.IonTestCase):
 
 
 class TestZTrivialPass(iontest.IonTestCase):
+    """This test is called ZTrivialPass to ensure that it runs AFTER
+    TestSpawnProcessesFail.
+    But there shouldn't be any coupling of state between unittests!, you
+    say... well, unfortunately, there is here :(
+    If these tests fail when run after TestSpawnProcessesFail, then there
+    is a problem with self._spawn_processes handling errors.
+    """
 
     @defer.inlineCallbacks
     def setUp(self):
@@ -60,9 +74,26 @@ class TestZTrivialPass(iontest.IonTestCase):
 
 
 class TestSpawnProcessesFail(iontest.IonTestCase):
+    """Test that an error in _spawn_processes will not cascade and cause
+    false errors for subsequent tests.
+
+    _spawn_processes will stop the container [twisted] service and then
+    propagate the initial error so that setUp (or whatever method it was
+    used in) fails.
+    """
 
     @defer.inlineCallbacks
     def test_a_setUp(self):
+        """
+        _spawn_processes is usually called in self.setUp
+        It is impossible to treat setUp like a test case, so this test case
+        does what you would want to usually do in setUp.
+        This test is called test_a_setUp to ensure it runs BEFORE the
+        test_znoop tests.
+        If the noop tests pass after test_a_setUp, then that means
+        cascading errors should not be caused by the _spawn_processes
+        function.
+        """
         services = [
                 {
                     'name':'test',
@@ -74,9 +105,15 @@ class TestSpawnProcessesFail(iontest.IonTestCase):
         yield self.failUnlessFailure(self._spawn_processes(services), ImportError)
 
     def test_znoop1(self):
+        """dummy test to run after the container has an error during the
+        setUp stage of a previous test run.
+        """
         print 'NOOP 1'
 
     def test_znoop2(self):
+        """dummy test to run after the container has an error during the
+        setUp stage of a previous test run.
+        """
         print 'NOOP 2'
 
 
