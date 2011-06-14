4e16734ba58bb51676ebed0e6f6116c5cb80ab05
Alon Yaari <ayaari@ucsd.edu>
2011-06-13 20:06:48 -0700

    Resolves JIRA OOIION-68 and OOIION-82.  New GPS simulator is entirely in python and needs no internet connection.  NMEA checksum error resolved.

--- ion/agents/instrumentagents/simulators/sim_NMEA0183_preplanned.py
+++ ion/agents/instrumentagents/simulators/sim_NMEA0183_preplanned.py
@@ -1,2 +1,119 @@
-__author__ = 'alon'
-  
+#!/usr/bin/env python
+"""
+@file ion/agents/instrumentagents/simulators/sim_NMEA0183_preplanned.py
+@brief Launches a simulated GPS that outputs NMEA0183, sourced from a predefined route.
+@author Alon Yaari
+"""
+
+
+from datetime import datetime
+from twisted.internet import task, reactor
+from twisted.internet.serialport import SerialPort
+from sim_NMEA0183 import *
+from gpsSimPath import simPath
+from twisted.protocols import basic
+
+GPSDEBUG = True     # Set true for debug output
+PREPLAN_MAX = 500
+
+
+class NMEASimProtocol (basic.LineReceiver):
+    """
+    """
+
+    def lineReceived (self, line):
+        NMEA0183SimPrePlanned.SimWriteToSerial (line)
+    
+
+class NMEA0183SimPrePlanned (NMEA0183SimBase):
+    """
+    Provide a simulated NMEA0183 GPS through a local virtual serial port.
+    """
+
+    WHICHSIM = 'NMEA0183 GPS Preplanned Route Simulator'
+
+    def SimGPSSetup(self):
+        """
+        Initialize preplanned route GPS simulator
+        @param None
+        @retval None
+        """
+
+        GPSDebug ('Setup: Preplanned NMEA0183 Simulator')
+        
+        self._workingSim = False
+        if not self._goodComms:
+            GPSDebug('Could not start NMEA0183 simulator: no serial comms.')
+            return
+
+        # Connect to the master serial port
+        GPSDebug ('Opening %s for simulator to connect with.' % self._serMaster)
+        baud = 57600
+        try:
+
+            self._s = SerialPort (NMEASimProtocol(),
+                                  self._serMaster,
+                                  reactor,
+                                  baudrate = 57600)
+        except Exception, e:
+            GPSDebug ('Failure: ' + e.__str__())
+            return
+        GPSDebug ('Successfuly opened  %s.' % self._serMaster)
+        time.sleep (1)
+        self._workingSim = True
+        self._goodComms = True
+
+        # Launch a reactor task to generate NMEA output each second
+        self._refSecs = -1
+        self._simRun = task.LoopingCall (self.NMEAOutput)
+        self._simRun.start (1.0)
+        reactor.run()
+
+    def NMEAOutput(self):
+        """
+        Called each second to produce NMEA output.
+            - Looks up values in a predefined table
+        """
+
+        # Manage the index
+        self._refSecs += 1
+        if self._refSecs > PREPLAN_MAX:
+            self._refSecs == 0
+
+        lat = simPath[self._refSecs]['lat']
+        lon = simPath[self._refSecs]['lon']
+        curTime = datetime.utcnow()
+        nmeaOut = BuildGPGGA ({'lat': lat, 'lon': lon, 'time': curTime})
+        self.SimWriteToSerial (nmeaOut)
+
+    def SimWriteToSerial(self, toWrite):
+        """
+        """
+        if not self.IsSimOK():
+            self._workingSim = False
+            GPSDebug ('NMEA not written: no working simulator')
+            return
+        if not self.IsSocatRunning():
+            self._goodComms = False
+            GPSDebug ('NMEA not written: socat not running')
+            return
+        self._s.write (toWrite)
+        GPSDebug ('NMEA written: %s' % toWrite)
+
+    def SimShutdown(self):
+        """
+        Stop the GPS simulator from firing every second.
+        """
+
+        self._simRun.stop()
+        # reactor.run()
+        self._workingSim = False
+
+    def IsSimulatorRunning(self):
+        return self._workingSim
+
+
+
+
+#
+
--- ion/agents/instrumentagents/test/test_SimpleNMEA.py
+++ ion/agents/instrumentagents/test/test_SimpleNMEA.py
@@ -1,23 +1,47 @@
+# Simple NMEA test
+# Simple verification of NMEA parsing routines and simulators
 
+import ion.util.procutils as pu
+import ion.util.ionlog
+log = ion.util.ionlog.getLogger(__name__)
 
 import ion.agents.instrumentagents.helper_NMEA0183 as NMEA
 
-
 # Completely valid GPGGA string
+
+log.info ('Verify NMEA parsing with known good GPGGA sentence:')
 testNMEA = '$GPGGA,051950.00,3532.2080,N,12348.0348,W,1,09,07.9,0005.9,M,0042.9,M,0.0,0000*52'
 parseNMEA = NMEA.NMEAString(testNMEA)
 print '\nTest valid GPGGA MNEA string: %s ' % testNMEA
 print parseNMEA.GetNMEAData()
 
 # Completely valid dummy string
+log.info ('Verify NMEA parsing with defined dummy setence:')
 testNMEA = '$XXXXX,0'
 parseNMEA = NMEA.NMEAString(testNMEA)
 print '\nTest valid XXXXX (dummy) MNEA string: %s ' % testNMEA
 print parseNMEA.GetNMEAData()
 
 # Invalid GPGGA string checksum
+log.info ('Verify correct NMEA behavior when passed a bad NMEA checksum:')
 testNMEA = '$GPGGA,051950.00,3532.2080,N,12348.0348,W,1,09,07.9,0005.9,M,0042.9,M,0.0,0000*F2'
 parseNMEA = NMEA.NMEAString(testNMEA)
 print '\nTest GPGGA with bad checksum: %s ' % testNMEA
 print parseNMEA.GetNMEAData()
 
+# SELECT ONE SIMULATOR
+#   - Comment out the simulator not being used
+
+#from ion.agents.instrumentagents.simulators.sim_NMEA0183_liveSFBay \
+#   import NMEA0183SimliveSFBay as sim
+#log.info ('Using SF BAY LIVE AIS DATA simulator')
+
+from ion.agents.instrumentagents.simulators.sim_NMEA0183_preplanned \
+    import NMEA0183SimPrePlanned as sim
+log.info ('Using PREPLANNED ROUTE GPS Simulator')
+
+log.info ('----- Launching simulator:  ' + sim.WHICHSIM)
+_sim = sim()
+if _sim.IsSimOK():
+    log.info ('----- Simulator launched.')
+    
