031f6527d4d07cc44224f363ca4e29013c313079
Christopher Mueller <cmueller@asascience.com>
2011-08-12 18:04:48 -0400

    fixed a little bit of erroneous logic in the fix for OOIION-404

--- ion/integration/eoi/validation/cdm_validation_service.py
+++ ion/integration/eoi/validation/cdm_validation_service.py
@@ -116,19 +116,22 @@ class CdmValidationService(ServiceProcess):
             #fetch file
             surl = str(data_url)
             dasfile = yield getPage(surl + ".das")
+            das_result = {'cdm_result':True}
         except Exception, ex:
             log.error('Invalid URL: the dataset is not hosted on a DAP server and cannot be validated')
-            cdm_resp = {'cdm_result':False, 'exception':'Invalid URL: the dataset is not hosted on a DAP server and cannot be validated'}
+            das_result = {'cdm_result':False, 'exception':'Invalid URL: the dataset is not hosted on a DAP server and cannot be validated'}
 
         # Step 2: Validate the URL against the CDM Validator WebService
-        if cdm_resp['cdm_result']:
+        if das_result['cdm_result']:
             try:
                 cdm_output = yield self.validate_cdm(data_url)
                 cdm_resp = yield self.process_cdm_validation_output(cdm_output)
             except Exception, ex:
                 log.warn('CDM Validation or validation output processing failed:  Cause: %s' % str(ex))
                 cdm_resp = {'cdm_result':False, 'exception':'Could not perform CDM Validation.  Please check the CDM Validator configuration.  Inner exception: %s' % ex}
-        
+        else:
+        	cdm_resp=das_result
+        	
         # Step 3: Validate the URL against the CF Checks Script
         cf_resp = {}
         if cdm_resp['cdm_result']:
